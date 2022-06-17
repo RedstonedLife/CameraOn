c94578aaf34fbf8e8eb308d7ee6b280cf095abca
Tal A. Baskin <talbaskin.business@gmail.com>
2022-06-17 09:50:20 +0300

    Update

--- src/main/java/com/bss/inc/cameraon/utils/OSValidator.java
+++ src/main/java/com/bss/inc/cameraon/utils/OSValidator.java
@@ -1,4 +1,12 @@
 package com.bss.inc.cameraon.utils;
 
 public class OSValidator {
+
+    private static String OS = System.getProperty("os.name").toLowerCase();
+    public static boolean IS_WINDOWS = (OS.indexOf("win") >= 0);
+    public static boolean IS_MAC = (OS.indexOf("mac") >= 0);
+    public static boolean IS_UNIX = (OS.indexOf("nix") >= 0 || OS.indexOf("nux") >= 0 || OS.indexOf("aix") > 0);
+    public static boolean IS_SOLARIS = (OS.indexOf("sunos") >= 0);
+    public static boolean IS_BSD = (OS.indexOf("bsd") >= 0);
+
 }
