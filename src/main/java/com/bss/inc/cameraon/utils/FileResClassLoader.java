7811fa5c9769bc27aeacffb7b5dc693f9d665c88
Tal A. Baskin <talbaskin.business@gmail.com>
2022-06-22 22:33:10 +0300

    Update-f71a6f25-9973-4eb4-83e3-541448c4988e

--- src/main/java/com/bss/inc/cameraon/utils/FileResClassLoader.java
+++ src/main/java/com/bss/inc/cameraon/utils/FileResClassLoader.java
@@ -1,5 +1,26 @@
 package com.bss.inc.cameraon.utils;
 
 public class FileResClassLoader extends ClassLoader {
-    
+    private final transient File dataFolder;
+
+    public FileResClassLoader(final ClassLoader classLoader, final BNC instance) {
+        super(classLoader);
+        this.dataFolder = new File(ab.SETTINGS_FOLDER);
+    }
+
+    @Override
+    public URL getResource(final String string) {
+        final File file = new File(dataFolder, string);
+        if (file.exists())
+            try {return file.toURI().toURL();} catch (final MalformedURLException ignored) {}
+        return null;
+    }
+
+    @Override
+    public InputStream getResourceAsStream(final String string) {
+        final File file = new File(dataFolder, string);
+        if (file.exists())
+            try {return new FileInputStream(file);} catch (final FileNotFoundException ignored) {}
+        return null;
+    }
 }
