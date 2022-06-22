cc4e5d61238223c10ae2547612edd3f65573a9e6
Tal A. Baskin <talbaskin.business@gmail.com>
2022-06-22 22:38:32 +0300

    Update-dff2047f-3875-4120-a466-a0bca2a1f9dc

--- src/main/java/com/bss/inc/cameraon/utils/$UI$01.java
+++ src/main/java/com/bss/inc/cameraon/utils/$UI$01.java
@@ -17,17 +17,17 @@ public class $UI$01 {
     public static void u_00$1$1(SplitPane csplitpane) {csplitpane.getDividers().get(0).positionProperty().addListener((observableValue, number, t1) -> csplitpane.getDividers().get(0).setPosition(FrontendPaths.DIVID_POS));}
     public static void u_01$1$1(SplitPane csplitpane, Button[] buttons) {
         u_00$1$1(csplitpane);
-        buttons[0].setOnAction(e -> MainClass.getSCENE_MANAGER().changeDialogScene("mainWindow",MainClass.class.getResource(FrontendPaths.SETTINGS_FXML)));
-        buttons[1].setOnAction(e -> MainClass.getSCENE_MANAGER().changeDialogScene("mainWindow",MainClass.class.getResource(FrontendPaths.CAM_SETTINGS)));
-        buttons[2].setOnAction(e -> MainClass.getSCENE_MANAGER().changeDialogScene("mainWindow",MainClass.class.getResource(FrontendPaths.APPLY_FXML)));
-        buttons[3].setOnAction(e -> MainClass.getSCENE_MANAGER().changeDialogScene("mainWindow",MainClass.class.getResource(FrontendPaths.SCAN_FXML)));
-        buttons[4].setOnAction(e -> {if(MainClass.getSCENE_MANAGER().getDialog("popUpWindow").isEnabled()) {MainClass.getSCENE_MANAGER().getDialog("popUpWindow").closeDialog();} else {MainClass.getSCENE_MANAGER().getDialog("popUpWindow").openDialog();BNC.getSCENE_MANAGER().changeDialogScene("popUpWindow", MainClass.class.getResource(FrontendPaths.POP_HELP_FXML));}});
+        buttons[0].setOnAction(e -> MainClass.getWindowManager().changeDialogScene("mainWindow",MainClass.class.getResource(FrontendPaths.SETTINGS_FXML)));
+        buttons[1].setOnAction(e -> MainClass.getWindowManager().changeDialogScene("mainWindow",MainClass.class.getResource(FrontendPaths.CAM_SETTINGS)));
+        buttons[2].setOnAction(e -> MainClass.getWindowManager().changeDialogScene("mainWindow",MainClass.class.getResource(FrontendPaths.APPLY_FXML)));
+        buttons[3].setOnAction(e -> MainClass.getWindowManager().changeDialogScene("mainWindow",MainClass.class.getResource(FrontendPaths.SCAN_FXML)));
+        buttons[4].setOnAction(e -> {if(MainClass.getWindowManager().getDialog("popUpWindow").isEnabled()) {MainClass.getWindowManager().getDialog("popUpWindow").closeDialog();} else {MainClass.getWindowManager().getDialog("popUpWindow").openDialog();BNC.getWindowManager().changeDialogScene("popUpWindow", MainClass.class.getResource(FrontendPaths.POP_HELP_FXML));}});
     }
     public static void u_02$1$1(Map<Button, String> map) {for(Button object : map.keySet()) {object.setText(map.get(object));}}
     public static void u_03$1$1(Map<Text, String> map) {for(Text object : map.keySet()) {object.setText(map.get(object));}}
     public static void u_04$1$1(Button object) {object.setOnAction(e -> {
-        BNC.getSCENE_MANAGER().changeDialogScene("preferencesDialog", MainClass.class.getResource(FrontendPaths.PREF_DISPLAY_FXML)); // <--- Look at this closely;
-        BNC.getSCENE_MANAGER().openDialog("preferencesDialog");
+        BNC.getWindowManager().changeDialogScene("preferencesDialog", MainClass.class.getResource(FrontendPaths.PREF_DISPLAY_FXML)); // <--- Look at this closely;
+        BNC.getWindowManager().openDialog("preferencesDialog");
     });}
     public static <T> void u_05$1$1(ChoiceBox<T> cb, int dfv, T[] vl) {for(T v : vl) {cb.getItems().add(v);}cb.setValue(cb.getItems().get(dfv));}
 }
