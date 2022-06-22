61c0bc4e4f225a56eccb16ee09c1bcb24159cc0f
Tal A. Baskin <talbaskin.business@gmail.com>
2022-06-22 22:21:50 +0300

    Update-75580321-390f-4e8d-a8cc-c8d44d55261a

--- src/main/java/com/bss/inc/cameraon/i18n/II18n.java
+++ src/main/java/com/bss/inc/cameraon/i18n/II18n.java
@@ -1,9 +1,12 @@
 package com.bss.inc.cameraon.i18n;
 
+import java.util.Locale;
+
 public interface II18n {
     /**
      * Gets the current locale setting
      *
      * @return the current locale, if not set it will return the default locale
      */
+    Locale
 }
