10cf7c3dd1bf54af081a080f9ab526b8b19ae98d
Tal A. Baskin <talbaskin.business@gmail.com>
2022-06-22 22:41:51 +0300

    Update-327f5ff1-786e-4915-b851-8d710a317971

--- src/main/java/com/bss/inc/cameraon/display/controllers/Settings.java
+++ src/main/java/com/bss/inc/cameraon/display/controllers/Settings.java
@@ -2,9 +2,14 @@ package com.bss.inc.cameraon.display.controllers;
 
 import com.bss.inc.cameraon.settings.ISettings;
 
+import java.io.File;
+
 public class Settings implements ISettings {
 
+    private File file;
+
     
+
     @Override
     public void Setup() {
 
