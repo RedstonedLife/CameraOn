3539ea67832837a6375b285fa3e76125dcc6b4ef
Tal A. Baskin <talbaskin.business@gmail.com>
2022-06-22 22:22:28 +0300

    Update-81c5d029-385a-4a8d-8502-5c537d7c1991

--- src/main/java/com/bss/inc/cameraon/i18n/I18n.java
+++ src/main/java/com/bss/inc/cameraon/i18n/I18n.java
@@ -1,5 +1,8 @@
 package com.bss.inc.cameraon.i18n;
 
+import java.util.Locale;
+
 public class I18n implements II18n {
     private static I18n instance;
+    private final transient Locale defaultLocale = Locale.getDefault();
 }
