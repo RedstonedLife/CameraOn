3496ef06ba131fcc1789b3ffe0a6b14dc2f92da7
Tal A. Baskin <talbaskin.business@gmail.com>
2022-06-22 22:23:43 +0300

    Update-3a4188b0-c72c-4a50-9d3b-5d5115282561

--- src/main/java/com/bss/inc/cameraon/i18n/I18n.java
+++ src/main/java/com/bss/inc/cameraon/i18n/I18n.java
@@ -2,7 +2,10 @@ package com.bss.inc.cameraon.i18n;
 
 import com.bss.inc.cameraon.MainClass;
 
+import java.text.MessageFormat;
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 import java.util.ResourceBundle;
 
 public class I18n implements II18n {
@@ -11,4 +14,11 @@ public class I18n implements II18n {
     private transient ResourceBundle defaultBundle;
     private final transient MainClass _i;
     private transient Locale currentLocale = defaultLocale;
+    private transient ResourceBundle customBundle;
+    private transient ResourceBundle localeBundle;
+    private transient Map<String, MessageFormat> messageFormatCache = new HashMap<>();
+
+    public I18n(final MainClass instance) {
+        this._i = instance;
+    }
 }
