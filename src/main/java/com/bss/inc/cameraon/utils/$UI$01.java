ecb211a14ef719fb5377782f170afde120107bcc
Tal A. Baskin <talbaskin.business@gmail.com>
2022-06-23 12:16:14 +0300

    Update-675c8284-3856-4e76-a223-42e0743501b3

--- src/main/java/com/bss/inc/cameraon/utils/$UI$01.java
+++ src/main/java/com/bss/inc/cameraon/utils/$UI$01.java
@@ -1,6 +1,7 @@
 package com.bss.inc.cameraon.utils;
 
 import com.bss.inc.cameraon.MainClass;
+import com.bss.inc.cameraon.constants.FrontendPaths;
 import com.redsoftware.ltd.bnc.BNC;
 import com.redsoftware.ltd.bnc.constants.aa;
 import javafx.event.ActionEvent;
