c4995ad9f82a60783384508e1d798a715c0bb041
Tal A. Baskin <talbaskin.business@gmail.com>
2022-06-23 13:13:41 +0300

    Update-9253c46d-afbd-4132-b123-1a5800c4922f

--- src/main/java/com/bss/inc/cameraon/Launcher.java
+++ src/main/java/com/bss/inc/cameraon/Launcher.java
@@ -5,11 +5,23 @@ import com.bss.inc.cameraon.exceptions.UnknownOS;
 import com.bss.inc.cameraon.logging.LogManager;
 import com.bss.inc.cameraon.logging.Logger;
 
+import java.io.FileNotFoundException;
+
 public class Launcher {
     static LogManager _LM;
     public static void main(String[] args) throws UnknownOS {
         _LM = new LogManager();
         LogManager.addLogger("BNC", new Logger("BNC"));
         Settings settings = new Settings("test", "{}");
+        settings.setValue("TestValue", 2500);
+        settings.setValue("Float", 2.5f);
+        settings.setValue("Double", 2.55d);
+        settings.setValue("Bool", true);
+        settings.setValue("LongString","This is a long string");
+        try {
+            settings.Save();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
