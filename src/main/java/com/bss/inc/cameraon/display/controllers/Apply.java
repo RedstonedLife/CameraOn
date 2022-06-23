ec91604d402fb3944af59349d0aaac57b5c43e9a
Tal A. Baskin <talbaskin.business@gmail.com>
2022-06-23 13:59:26 +0300

    Update-2062c9ac-8de4-4cc8-9eb8-72a48b5124a7

--- src/main/java/com/bss/inc/cameraon/display/controllers/Apply.java
+++ src/main/java/com/bss/inc/cameraon/display/controllers/Apply.java
@@ -8,8 +8,8 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.SplitPane;
-import javafx.stage.Stage;
 import javafx.scene.text.Text;
+
 import java.net.URL;
 import java.util.ResourceBundle;
 
