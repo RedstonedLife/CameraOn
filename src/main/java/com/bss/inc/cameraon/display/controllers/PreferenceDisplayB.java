54e8c3a5bae49fe97596d6c9162482d00826bbe3
Tal A. Baskin <talbaskin.business@gmail.com>
2022-06-23 16:08:42 +0300

    Update-72aa28e2-7024-4b2e-8a41-25c4ecc22809

--- src/main/java/com/bss/inc/cameraon/display/controllers/PreferenceDisplayB.java
+++ src/main/java/com/bss/inc/cameraon/display/controllers/PreferenceDisplayB.java
@@ -1,4 +1,9 @@
 package com.bss.inc.cameraon.display.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class PreferenceDisplayB {
+    @FXML
+    private void closeApp(ActionEvent event) {instance.getWindowManager().closeDialog("preferencesDialog");}
 }
