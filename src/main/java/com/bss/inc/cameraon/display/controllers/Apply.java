2bc378ce00e9b32caea9c65dbba25c33065f3927
Tal A. Baskin <talbaskin.business@gmail.com>
2022-06-23 13:58:35 +0300

    Update-237734d4-31e4-494e-a06e-260c594bef82

--- src/main/java/com/bss/inc/cameraon/display/controllers/Apply.java
+++ src/main/java/com/bss/inc/cameraon/display/controllers/Apply.java
@@ -1,4 +1,38 @@
 package com.bss.inc.cameraon.display.controllers;
 
-public class Apply {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.SplitPane;
+import javafx.scene.text.Text;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+
+public class Apply implements Initializable {
+
+    @FXML private Button settingsSceneBtn,cctvSceneBtn,scanSceneBtn,applySceneBtn,HelpBtn;
+    @FXML private SplitPane cmainsplit;
+    @FXML private Text taskslefttxt,backendtxt,applystatustxt,versiontxt,licensetxt;
+
+    // # Close Window Button
+    @FXML private void closeApp(ActionEvent event) {BNC.getSCENE_MANAGER().closeDialogs();}
+
+    // # Minimize Window Button
+    @FXML private void minimizeApp(ActionEvent event) {BNC.getSCENE_MANAGER().minimizeDialog("mainWindow");}
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        $UI$01.u_01$1$1(cmainsplit,new Button[]{settingsSceneBtn,cctvSceneBtn,applySceneBtn,scanSceneBtn,HelpBtn});
+        // I18n
+        settingsSceneBtn.setText(tl("settings.btn.text"));
+        cctvSceneBtn.setText(tl("settings.cctv.btn.text"));
+        scanSceneBtn.setText(tl("scan.btn.text"));
+        applySceneBtn.setText(tl("apply.btn.text"));
+        taskslefttxt.setText(tl("apply.task.left"));
+        backendtxt.setText(tl("apply.task.default",""));
+        applystatustxt.setText(tl("apply.task.status"));
+        versiontxt.setText(tl("version.format",aa.VERSION_TEXT,aa.BUILD));
+        licensetxt.setText(tl("license.format",aa.LICENSEE,aa.LICENSEKEY));
+    }
+
 }
