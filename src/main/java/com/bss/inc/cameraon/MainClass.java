72ca1043094c738fdd485403337dc6d01dbd7b95
Tal A. Baskin <talbaskin.business@gmail.com>
2022-06-23 15:50:03 +0300

    Update-228a47e5-fc6a-47ef-ba62-a593343e936c

--- src/main/java/com/bss/inc/cameraon/MainClass.java
+++ src/main/java/com/bss/inc/cameraon/MainClass.java
@@ -21,30 +21,31 @@ public class MainClass extends Application {
     private static Class<MainClass> clazz = MainClass.class;
 
     static {
-        dialogs = new Dialog[] {
-          new Dialog(FrontendPaths.HEIGHT, FrontendPaths.WIDTH, "mainWindow", new URL[]{ // MainWindow URL[] Start
-                  clazz.getResource(FrontendPaths.SETTINGS_FXML),
-                  clazz.getResource(FrontendPaths.WELCOME_FXML), clazz.getResource(FrontendPaths.CAM_SETTINGS),
-                  clazz.getResource(FrontendPaths.SCAN_FXML), clazz.getResource(FrontendPaths.APPLY_FXML),
-          }), // MainWindow URL[] End
-          new Dialog(300d, 200d, "confirmationDialog", new URL[]{ // ConfirmationDialog URL[] Start
-                  clazz.getResource(FrontendPaths.CONFIRMATION_FXML), clazz.getResource(FrontendPaths.LANG_CONFIRMATION_FXML)
-          }), // ConfirmationDialog URL[] End
-          new Dialog(500d, 800d, "preferencesDialog", new URL[]{ // PreferencesDialog URL[] Start
-                  clazz.getResource(FrontendPaths.PREF_DISPLAY_FXML), clazz.getResource(FrontendPaths.PREF_DISPLAY_AS_FXML),
-                  clazz.getResource(FrontendPaths.PREF_DISPLAY_CBS_FXML), clazz.getResource(FrontendPaths.PREF_DISPLAY_IBS_FXML)
-          }), // PreferencesDialog URL[] End
-          new Dialog(350d, 500d, "popUpWindow", new URL[] { // PopUpWindow URL[] Start
-            clazz.getResource(FrontendPaths.POP_FAQ_FXML),clazz.getResource(FrontendPaths.POP_TPL_FXML),
-                  clazz.getResource(FrontendPaths.POP_FAQ_FXML)
-          }) // PopUpWindow URL[] End
-        };
+
     }
 
     @Override
     public void start(Stage stage) {
         this._i18n.OnStart();
-        try {WindowManager = new DialogManager(dialogs);}
+
+        try {WindowManager = new DialogManager(new Dialog[] {
+                new Dialog(FrontendPaths.HEIGHT, FrontendPaths.WIDTH, "mainWindow", new URL[]{ // MainWindow URL[] Start
+                        clazz.getResource(FrontendPaths.SETTINGS_FXML),
+                        clazz.getResource(FrontendPaths.WELCOME_FXML), clazz.getResource(FrontendPaths.CAM_SETTINGS),
+                        clazz.getResource(FrontendPaths.SCAN_FXML), clazz.getResource(FrontendPaths.APPLY_FXML),
+                }), // MainWindow URL[] End
+                new Dialog(300d, 200d, "confirmationDialog", new URL[]{ // ConfirmationDialog URL[] Start
+                        clazz.getResource(FrontendPaths.CONFIRMATION_FXML), clazz.getResource(FrontendPaths.LANG_CONFIRMATION_FXML)
+                }), // ConfirmationDialog URL[] End
+                new Dialog(500d, 800d, "preferencesDialog", new URL[]{ // PreferencesDialog URL[] Start
+                        clazz.getResource(FrontendPaths.PREF_DISPLAY_FXML), clazz.getResource(FrontendPaths.PREF_DISPLAY_AS_FXML),
+                        clazz.getResource(FrontendPaths.PREF_DISPLAY_CBS_FXML), clazz.getResource(FrontendPaths.PREF_DISPLAY_IBS_FXML)
+                }), // PreferencesDialog URL[] End
+                new Dialog(350d, 500d, "popUpWindow", new URL[] { // PopUpWindow URL[] Start
+                        clazz.getResource(FrontendPaths.POP_FAQ_FXML),clazz.getResource(FrontendPaths.POP_TPL_FXML),
+                        clazz.getResource(FrontendPaths.POP_FAQ_FXML)
+                }) // PopUpWindow URL[] End
+        });}
         catch (IOException e) {logger.error("Error occurred while initializing the WindowManager", e);}
         WindowManager.changeDialogScene("mainWindow", clazz.getResource(FrontendPaths.SETTINGS_FXML));
         WindowManager.openDialog("mainWindow");
