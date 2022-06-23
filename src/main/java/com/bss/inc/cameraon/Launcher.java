ce4da76ac81e3d2c0c7c8901add3abbdae3444c8
Tal A. Baskin <talbaskin.business@gmail.com>
2022-06-23 13:02:25 +0300

    Update-00dfc70e-2a13-49f4-9b48-2c7294f84408

--- src/main/java/com/bss/inc/cameraon/Launcher.java
+++ src/main/java/com/bss/inc/cameraon/Launcher.java
@@ -2,10 +2,13 @@ package com.bss.inc.cameraon;
 
 import com.bss.inc.cameraon.display.controllers.Settings;
 import com.bss.inc.cameraon.exceptions.UnknownOS;
+import com.bss.inc.cameraon.logging.LogManager;
+import com.bss.inc.cameraon.logging.Logger;
 
 public class Launcher {
     public static void main(String[] args) throws UnknownOS {
-        //MainClass.launch(args);
+        _LM = new LogManager();
+        LogManager.addLogger("BNC", new Logger("BNC"));
         Settings settings = new Settings("test", "{}");
     }
 }
