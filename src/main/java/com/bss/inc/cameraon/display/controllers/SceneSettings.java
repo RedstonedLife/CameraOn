7dc86653bc758438a00cf3ca8be134e1897a03d2
Tal A. Baskin <talbaskin.business@gmail.com>
2022-06-23 18:11:58 +0300

    Update-82b69321-08ae-4ab1-8186-2d5a76de262a

--- src/main/java/com/bss/inc/cameraon/display/controllers/SceneSettings.java
+++ src/main/java/com/bss/inc/cameraon/display/controllers/SceneSettings.java
@@ -20,4 +20,32 @@ public class SceneSettings {
     @FXML private void closeApp(ActionEvent event) {MainClass.getWindowManager().closeDialogs();}
     // # Minimize Window Button
     @FXML private void minimizeApp(ActionEvent event) {MainClass.getWindowManager().minimizeDialog("mainWindow");}
+
+    private void initializeLanguage() {
+        settingsSceneBtn.setText(tl("settings.btn.text"));
+        cctvSceneBtn.setText(tl("settings.cctv.btn.text"));
+        scanSceneBtn.setText(tl("scan.btn.text"));
+        applySceneBtn.setText(tl("apply.btn.text"));
+        languagetxt.setText(tl("settings.language.text"));
+        versiontxt.setText(tl("version.format",aa.VERSION_TEXT,aa.BUILD));
+        licensetxt.setText(tl("license.format",aa.LICENSEE,aa.LICENSEKEY));
+        langCb.setOnAction(e -> {
+            BNC.getSettingsManager().set_language(langCb.getItems().indexOf(langCb.getValue()) + 1);
+            BNC.getSCENE_MANAGER().changeDialogScene("confirmationDialog", BNC.class.getResource(aa.LANG_CONFIRMATION_FXML));
+            BNC.getSCENE_MANAGER().openDialog("confirmationDialog");
+        });
+    }
+
+    public void modifyStyle() {
+        this.languagetxt.getStyleClass().add("text");
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        langCb.getItems().addAll(tl("language.english"),tl("language.russian"),tl("language.hebrew"),tl("language.german"),tl("language.spanish"),tl("language.ukrainian"),
+                tl("language.arabic"),tl("language.french"),tl("language.polish"),tl("language.portuguese"),tl("language.czech"),tl("language.italian"),tl("language.romanian"),tl("language.greek"));
+        langCb.setValue(langCb.getItems().get(BNC.getSettingsManager().get_language()==0 ? BNC.getSettingsManager().get_language() : BNC.getSettingsManager().get_language()-1));
+        $UI$01.u_01$1$1(cmainsplit,new Button[]{settingsSceneBtn,cctvSceneBtn,applySceneBtn,scanSceneBtn,HelpBtn});
+        initializeLanguage();
+    }
 }
