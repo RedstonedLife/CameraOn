09a51235ea1f88dee6df6b340269812149cbabd4
Tal A. Baskin <talbaskin.business@gmail.com>
2022-06-23 16:03:22 +0300

    Update-7e2b1f89-22c1-4d5c-8873-a6d117407cff

--- src/main/java/com/bss/inc/cameraon/MainClass.java
+++ src/main/java/com/bss/inc/cameraon/MainClass.java
@@ -26,7 +26,7 @@ public class MainClass extends Application {
     @Override
     public void start(Stage stage) {
         this._i18n.OnStart();
-
+        this._i18n.updateLocale("en");
         try {WindowManager = new DialogManager(new Dialog[] {
                 new Dialog(FrontendPaths.HEIGHT, FrontendPaths.WIDTH, "mainWindow", new URL[]{ // MainWindow URL[] Start
                         clazz.getResource(FrontendPaths.SETTINGS_FXML),
