dd6441bc56c75d2b3c25ca943282ea2c36e4253a
Tal A. Baskin <talbaskin.business@gmail.com>
2022-06-23 12:43:11 +0300

    Update-9cd2e43b-f1f9-4bc8-aecc-25e93f0a8a76

--- src/main/java/com/bss/inc/cameraon/settings/ISettings.java
+++ src/main/java/com/bss/inc/cameraon/settings/ISettings.java
@@ -1,8 +1,12 @@
 package com.bss.inc.cameraon.settings;
 
+import org.json.JSONException;
+
+import java.io.FileNotFoundException;
+
 public interface ISettings {
     void Setup();
-    void Save();
+    void Save() throws FileNotFoundException, JSONException;
     void Load();
     <T> T getValue(String key);
     <T> void setValue(String key, T value);
