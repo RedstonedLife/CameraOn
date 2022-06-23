3a45616385e62b13b2aedd4d7daacba896d901c8
Tal A. Baskin <talbaskin.business@gmail.com>
2022-06-23 15:46:16 +0300

    Update-2927e321-9501-4a25-9106-eccb646e0b0a

--- src/main/java/com/bss/inc/cameraon/display/controllers/Scan.java
+++ src/main/java/com/bss/inc/cameraon/display/controllers/Scan.java
@@ -1,4 +1,18 @@
 package com.bss.inc.cameraon.display.controllers;
 
-public class Scan {
+import com.bss.inc.cameraon.MainClass;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+public class Scan implements Initializable {
+
+    // # Close Window Button
+    @FXML
+    private void closeApp(ActionEvent event) {
+        MainClass.getWindowManager().closeDialogs();}
+
+    // # Minimize Window Button
+    @FXML private void minimizeApp(ActionEvent event) {MainClass.getWindowManager().minimizeDialog("mainWindow");}
+
 }
