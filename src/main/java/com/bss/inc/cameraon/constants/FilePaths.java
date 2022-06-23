ace214887966bb1f3f263c25d3a25b6fe2582fbf
Tal A. Baskin <talbaskin.business@gmail.com>
2022-06-23 13:00:39 +0300

    Update-49058b53-4827-4f7b-9f85-77be33ba3186

--- src/main/java/com/bss/inc/cameraon/constants/FilePaths.java
+++ src/main/java/com/bss/inc/cameraon/constants/FilePaths.java
@@ -3,9 +3,9 @@ package com.bss.inc.cameraon.constants;
 import com.bss.inc.cameraon.utils.OSValidator;
 
 public class FilePaths {
-    public static final String UNIX_CONFIG_LOCATION = "/.config/BNC/";
-    public static final String WIN_CONFIG_LOCATION = "/AppData/Local/BNC/";
-    public static final String MAC_CONFIG_LOCATION = "/Library/ApplicationSupport/BNC/";
+    public static final String UNIX_CONFIG_LOCATION = "/.config/BaskinSoftwareSolutions/CameraOn/";
+    public static final String WIN_CONFIG_LOCATION = "/AppData/Local/BaskinSoftwareSolutions/CameraOn/";
+    public static final String MAC_CONFIG_LOCATION = "/Library/ApplicationSupport/BaskinSoftwareSolutions/CameraOn/";
     public static final String HOME_LOCATION = System.getProperty("user.home");
     public static final String SETTINGS_FOLDER = OSValidator.IS_WINDOWS ? HOME_LOCATION + WIN_CONFIG_LOCATION :
             OSValidator.IS_UNIX ? HOME_LOCATION + UNIX_CONFIG_LOCATION : OSValidator.IS_MAC ?
