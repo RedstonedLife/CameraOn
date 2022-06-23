4253f8142327769c01b20e7f052ca109e12b7baa
Tal A. Baskin <talbaskin.business@gmail.com>
2022-06-23 13:38:26 +0300

    Update-2c4c086f-87b3-4c26-a812-a3ed8c009aa3

--- src/main/java/com/bss/inc/cameraon/MainClass.java
+++ src/main/java/com/bss/inc/cameraon/MainClass.java
@@ -19,7 +19,7 @@ public class MainClass extends Application {
     public void stop() {}
 
     public static void main(String[] args) {
-        this._i18n.OnStart();
+        _i18n.OnStart();
         launch();
     }
 
