4ebe3931ac3485f16bd4371fbbe67fc57b5b6b5c
Tal A. Baskin <talbaskin.business@gmail.com>
2022-06-23 12:57:13 +0300

    Update-86849e6b-dafb-4386-9f5b-fc339b5d5547

--- src/main/java/com/bss/inc/cameraon/Launcher.java
+++ src/main/java/com/bss/inc/cameraon/Launcher.java
@@ -1,6 +1,11 @@
 package com.bss.inc.cameraon;
+
+import com.bss.inc.cameraon.display.controllers.Settings;
+import com.bss.inc.cameraon.exceptions.UnknownOS;
+
 public class Launcher {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnknownOS {
         //MainClass.launch(args);
+        Settings settings = new Settings("test", "{}");
     }
 }
