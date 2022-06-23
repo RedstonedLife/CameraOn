16b3270f0f180581cca247147326743791aeacdd
Tal A. Baskin <talbaskin.business@gmail.com>
2022-06-23 16:05:53 +0300

    Update-7112ff9e-d365-43b0-b1af-92e49bb3371f

--- src/main/java/com/bss/inc/cameraon/display/controllers/CameraSettings.java
+++ src/main/java/com/bss/inc/cameraon/display/controllers/CameraSettings.java
@@ -1,4 +1,55 @@
 package com.bss.inc.cameraon.display.controllers;
 
+import javafx.scene.control.Button;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import static com.bss.inc.cameraon.i18n.I18n.tl;
+
 public class CameraSettings {
+    @FXML private Button settingsSceneBtn,cctvSceneBtn,scanSceneBtn,applySceneBtn,btnClose,btnMinimize,HelpBtn;
+    @FXML private Button fpsleftbtn,fpsDleftbtn,fpsrightbtn,fpsDrightbtn;
+    @FXML private ChoiceBox<String> veCB,rCB,brCB;
+    @FXML private SplitPane cmainsplit;
+    @FXML private Text cctvencodingtxt,cctvencodingbetxt,cctvfpstxt,cctvrestxt,cctvbrtxt,fpscounttxt,versiontxt,licensetxt;
+
+    // # Close Window Button
+    @FXML private void closeApp(ActionEvent event) {instance.getSCENE_MANAGER().closeDialogs();}
+
+    // # Minimize Window Button
+    @FXML private void minimizeApp(ActionEvent event) {instance.getSCENE_MANAGER().minimizeDialog("mainWindow");}
+
+    private BNC instance = new BNC();
+
+    private void initializeLanguage() {
+        cctvencodingtxt.setText(tl("settings.cctv.encoding"));
+        cctvencodingbetxt.setText(tl("settings.cctv.encoding.below"));
+        cctvbrtxt.setText(tl("settings.cctv.bitrate"));
+        cctvfpstxt.setText(tl("settings.cctv.fps"));
+        settingsSceneBtn.setText(tl("settings.btn.text"));
+        cctvSceneBtn.setText(tl("settings.cctv.btn.text"));
+        scanSceneBtn.setText(tl("scan.btn.text"));
+        applySceneBtn.setText(tl("apply.btn.text"));
+        versiontxt.setText(tl("version.format",FrontendPaths.VERSION_TEXT,FrontendPaths.BUILD));
+        licensetxt.setText(tl("license.format",FrontendPaths.LICENSEE,FrontendPaths.LICENSEKEY));
+    }
+
+    private void modifyStyle() {
+        this.cctvencodingtxt.getStyleClass().add("text");
+        this.cctvencodingbetxt.getStyleClass().add("text");
+        this.cctvbrtxt.getStyleClass().add("text");
+        this.cctvfpstxt.getStyleClass().add("text");
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        initializeLanguage();
+        modifyStyle();
+        $UI$01.u_01$1$1(cmainsplit, new Button[]{settingsSceneBtn, cctvSceneBtn, applySceneBtn, scanSceneBtn, HelpBtn});
+        /* Load Choice Boxes */
+        veCB.getItems().addAll("H.264", "H.264H", "H.265");
+        rCB.getItems().addAll("1080 (1920x1080)", "720 (1280x720)", "480 (720x480)", "360 (640x360)");
+        brCB.getItems().addAll("4096 Kb/s (4 Mb/s)", "3072 Kb/s (3 Mb/s)", "2048 Kb/s (2 Mb/s)", "1024 Kb/s (1 Mb/s)");
+    }
 }
