63c5b76be802bb1ff623400eabddb3c04ae56d0e
Tal A. Baskin <talbaskin.business@gmail.com>
2022-06-23 18:12:57 +0300

    Update-adacb6b4-7fb8-4a2f-bf40-7f86b9be2632

--- src/main/java/com/bss/inc/cameraon/display/controllers/SceneSettings.java
+++ src/main/java/com/bss/inc/cameraon/display/controllers/SceneSettings.java
@@ -51,7 +51,7 @@ public class SceneSettings {
     public void initialize(URL url, ResourceBundle resourceBundle) {
         langCb.getItems().addAll(tl("language.english"),tl("language.russian"),tl("language.hebrew"),tl("language.german"),tl("language.spanish"),tl("language.ukrainian"),
                 tl("language.arabic"),tl("language.french"),tl("language.polish"),tl("language.portuguese"),tl("language.czech"),tl("language.italian"),tl("language.romanian"),tl("language.greek"));
-        langCb.setValue(langCb.getItems().get(BNC.getSettingsManager().get_language()==0 ? BNC.getSettingsManager().get_language() : BNC.getSettingsManager().get_language()-1));
+        langCb.setValue(0);
         $UI$01.u_01$1$1(cmainsplit,new Button[]{settingsSceneBtn,cctvSceneBtn,applySceneBtn,scanSceneBtn,HelpBtn});
         initializeLanguage();
     }
