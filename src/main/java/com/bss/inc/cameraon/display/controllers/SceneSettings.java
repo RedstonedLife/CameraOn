4869b9086a18c713fb1a2f54c412860ea37603eb
Tal A. Baskin <talbaskin.business@gmail.com>
2022-06-23 16:10:05 +0300

    Update-5834cc43-f86a-4451-a198-5f7dd592ab13

--- src/main/java/com/bss/inc/cameraon/display/controllers/SceneSettings.java
+++ src/main/java/com/bss/inc/cameraon/display/controllers/SceneSettings.java
@@ -1,4 +1,24 @@
 package com.bss.inc.cameraon.display.controllers;
 
+import com.bss.inc.cameraon.MainClass;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.SplitPane;
+import javafx.scene.text.Text;
+
 public class SceneSettings {
+    @FXML private ChoiceBox<String> langCb;
+    @FXML private SplitPane cmainsplit;
+    @FXML private Button settingsSceneBtn,cctvSceneBtn,scanSceneBtn,applySceneBtn,HelpBtn;
+    @FXML private Text languagetxt,versiontxt,licensetxt;
+    @FXML private Button btnClose;
+    @FXML private Button btnMinimize;
+
+    // # Close Window Button
+    @FXML private void closeApp(ActionEvent event) {
+        MainClass.getSCENE_MANAGER().closeDialogs();}
+    // # Minimize Window Button
+    @FXML private void minimizeApp(ActionEvent event) {BNC.getSCENE_MANAGER().minimizeDialog("mainWindow");}
 }
