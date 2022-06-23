8044b5663318af22566b9eb2253693dcdc1c108c
Tal A. Baskin <talbaskin.business@gmail.com>
2022-06-23 16:36:44 +0300

    Update-ab93848a-e31b-4e14-a782-0a429effee2e

--- src/main/java/com/bss/inc/cameraon/display/controllers/LanguageConfirmation.java
+++ src/main/java/com/bss/inc/cameraon/display/controllers/LanguageConfirmation.java
@@ -1,5 +1,10 @@
 package com.bss.inc.cameraon.display.controllers;
 
+import com.bss.inc.cameraon.MainClass;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class LanguageConfirmation {
-    @FXML private void closeApp(ActionEvent event) {instance.getSCENE_MANAGER().closeDialog("confirmationDialog");}
+    @FXML
+    private void closeApp(ActionEvent event) {MainClass.getSCENE_MANAGER().closeDialog("confirmationDialog");}
 }
