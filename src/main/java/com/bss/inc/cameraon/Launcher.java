0e2c945b4d5bc7ee1e8e612c015d358bc12db964
Tal A. Baskin <talbaskin.business@gmail.com>
2022-06-23 14:32:58 +0300

    Update-88f37614-a901-4ed1-a75f-67200c5d62fc

--- src/main/java/com/bss/inc/cameraon/Launcher.java
+++ src/main/java/com/bss/inc/cameraon/Launcher.java
@@ -1,11 +1,9 @@
 package com.bss.inc.cameraon;
 
-import com.bss.inc.cameraon.display.controllers.Settings;
 import com.bss.inc.cameraon.exceptions.UnknownOS;
 import com.bss.inc.cameraon.logging.LogManager;
 import com.bss.inc.cameraon.logging.Logger;
 
-import java.io.FileNotFoundException;
 import java.time.LocalDateTime;
 
 public class Launcher {
