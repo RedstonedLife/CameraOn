238c5df8b49c7d4ffbf3e8f28b43a454521008ac
Tal A. Baskin <talbaskin.business@gmail.com>
2022-06-23 13:37:34 +0300

    Update-6f836897-1214-4a7f-b2a8-8ec432efce2c

--- src/main/java/com/bss/inc/cameraon/MainClass.java
+++ src/main/java/com/bss/inc/cameraon/MainClass.java
@@ -1,6 +1,7 @@
 package com.bss.inc.cameraon;
 
 import com.bss.inc.cameraon.display.DialogManager;
+import com.bss.inc.cameraon.i18n.I18n;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
@@ -10,7 +11,7 @@ public class MainClass extends Application {
 
     public static final LocalDateTime _INIT_TIME = LocalDateTime.now();
     private static DialogManager WindowManager;
-    private transiet I18n i18n
+    private transiet I18n _i18n = new I18n(this);
 
     @Override
     public void start(Stage stage) {}
