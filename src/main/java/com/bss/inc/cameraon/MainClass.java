04e2d3f988c0a386b064ea8b4af1581406081ec4
Tal A. Baskin <talbaskin.business@gmail.com>
2022-06-26 17:53:57 +0300

    Update-8bc5a341-8b06-4b7b-91c5-13cfecf28178

--- src/main/java/com/bss/inc/cameraon/MainClass.java
+++ src/main/java/com/bss/inc/cameraon/MainClass.java
@@ -44,7 +44,6 @@ public class MainClass extends Application {
         catch (IOException e) {logger.error("Error occurred while initializing the WindowManager", e);}
         WindowManager.changeDialogScene("mainWindow",clazz.getResource(FrontendPaths.SETTINGS_FXML));
         WindowManager.openDialog("mainWindow");
-        stage.setTitle("CameraOn");
         //WindowManager.changeDialogScene("mainWindow", clazz.getResource(FrontendPaths.SETTINGS_FXML));
         //WindowManager.openDialog("mainWindow");
     }
