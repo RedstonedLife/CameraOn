4e83f4bca1ba14764d66b0b505ff756bfb84d61f
Tal A. Baskin <talbaskin.business@gmail.com>
2022-06-26 16:01:25 +0300

    Update-624651ce-b4cc-4ea6-b0de-0ed6ec4949b4

--- src/main/java/com/bss/inc/cameraon/display/controllers/QM.java
+++ src/main/java/com/bss/inc/cameraon/display/controllers/QM.java
@@ -1,5 +1,23 @@
 package com.bss.inc.cameraon.display.controllers;
 
-public class QM implements {
+import javafx.fxml.Initializable;
+
+public class QM implements Initializable {
+
+    @FXML
+    private Text qmHelpTxt,qmFaqTxt,qmTplTxt;
+    @FXML private Button qmHelpBtn,qmFaqBtn,qmTplBtn;
+
+    private BNC instance = new BNC();
+
+    @FXML private void closeApp(ActionEvent event) {instance.getSCENE_MANAGER().closeDialog("popUpWindow");}
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        //tl("language.{}.changeConfirmation".replace("{}",prLang(instance.getSCENE_MANAGER().getCurrentLanguage())),prLang(instance.getSCENE_MANAGER().getSettings().get_language()))
+        qmHelpBtn.setOnAction(e -> instance.getSCENE_MANAGER().changeDialogScene("popUpWindow", BNC.class.getResource(aa.POP_HELP_FXML)));
+        qmFaqBtn.setOnAction(e -> instance.getSCENE_MANAGER().changeDialogScene("popUpWindow", BNC.class.getResource(aa.POP_FAQ_FXML)));
+        qmTplBtn.setOnAction(e -> instance.getSCENE_MANAGER().changeDialogScene("popUpWindow", BNC.class.getResource(aa.POP_TPL_FXML)));
+    }
 
 }
