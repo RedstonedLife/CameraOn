90dac24144d59979db72f6db6d37b63af4aedc6d
Tal A. Baskin <talbaskin.business@gmail.com>
2022-06-26 16:06:10 +0300

    Update-08f05fa6-e1a1-4e45-9820-ae6e6d710292

--- src/main/java/com/bss/inc/cameraon/display/controllers/PreferenceDisplayB.java
+++ src/main/java/com/bss/inc/cameraon/display/controllers/PreferenceDisplayB.java
@@ -1,9 +1,10 @@
 package com.bss.inc.cameraon.display.controllers;
 
 import com.bss.inc.cameraon.MainClass;
+import com.bss.inc.cameraon.utils.$UI$01;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
 public class PreferenceDisplayB {
-    @FXML private void closeApp(ActionEvent event) {MainClass.getWindowManager().getDialog("mainWindow").changeScene(MainClass.getWindowManager().getDialog("mainWindow").getCurrentScene());}
+    @FXML private void closeApp(ActionEvent event) {$UI$01.u_06$1$1();}
 }
