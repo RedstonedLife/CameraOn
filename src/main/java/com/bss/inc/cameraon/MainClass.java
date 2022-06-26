66c7ac9985a843acf255639c913870a554e9dbf3
Tal A. Baskin <talbaskin.business@gmail.com>
2022-06-26 17:52:43 +0300

    Update-67f2726a-aa60-4ef9-a14b-06c8bc843624

--- src/main/java/com/bss/inc/cameraon/MainClass.java
+++ src/main/java/com/bss/inc/cameraon/MainClass.java
@@ -16,7 +16,6 @@ public class MainClass extends Application {
 
     private static Logger logger = Logger.getLogger("BNC");
     private static DialogManager WindowManager;
-    private static Dialog MainDialog;
     private transient I18n _i18n = new I18n(this);
     private static Class<MainClass> clazz = MainClass.class;
 
