12961fa55c6a5f511957a877b2ac5b5f6dc2b17d
Tal A. Baskin <talbaskin.business@gmail.com>
2022-06-30 20:48:22 +0300

    Update-00e7addc-38cc-4794-93a9-1da3326f0d28

--- src/main/java/com/bss/inc/cameraon/utils/$UI$01.java
+++ src/main/java/com/bss/inc/cameraon/utils/$UI$01.java
@@ -53,7 +53,7 @@ public class $UI$01 {
         buttons[0].setOnAction(e -> {
             if(fps.get() <= 5 || fps.get() -5<=5) return;
             else {
-                fps.set(-5);
+                fps=fps-5;
                 _j.put("frameRate", fps.get() -5);
                 Launcher.SettingsContainer.setValue("cameraSettings",_j);
                 try {Launcher.SettingsContainer.Save();} catch (FileNotFoundException ex) {ex.printStackTrace();}
