b6be10f9fd939d917aad6af83dbb08706ae42262
Tal A. Baskin <talbaskin.business@gmail.com>
2022-06-30 20:16:20 +0300

    Update-fbdd1a4b-3f9e-435f-897c-b357b05da2e6

--- src/main/java/com/bss/inc/cameraon/display/controllers/CameraSettings.java
+++ src/main/java/com/bss/inc/cameraon/display/controllers/CameraSettings.java
@@ -67,6 +67,6 @@ public class CameraSettings implements Initializable {
         int[] cb = new int[] {settings.getInt("resolution"), settings.getInt("bitrate"), settings.getInt("videnc")};
         veCB.setValue(veCB.getItems().get(cb[2]));
         rCB.setValue(rCB.getItems().get(cb[0]));
-        
+        brCB.setValue(brCB.getItems().get(cb[1]));
     }
 }
