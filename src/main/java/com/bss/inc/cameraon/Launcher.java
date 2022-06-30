8688fe565c24d12f24a4914dd03b06fcd4cb3f3f
Tal A. Baskin <talbaskin.business@gmail.com>
2022-06-30 19:47:15 +0300

    Update-6f67d34d-624c-4bc4-9512-e39f78cbd1da

--- src/main/java/com/bss/inc/cameraon/Launcher.java
+++ src/main/java/com/bss/inc/cameraon/Launcher.java
@@ -14,7 +14,7 @@ public class Launcher {
 
     static {
         try {
-            SettingsContainer = new Settings("app.settings","{\"appLanguage\":0,\"}");
+            SettingsContainer = new Settings("app.settings","{\"appLanguage\":0,\"cameraSettings}");
         } catch (UnknownOS e) {
             throw new RuntimeException(e);
         }
