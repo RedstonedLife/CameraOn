3c38b9db25024b5ed5dc961d0a3479c6843b8934
Tal A. Baskin <talbaskin.business@gmail.com>
2022-06-30 20:33:57 +0300

    Update-c51fd06b-966b-45b0-b155-f834869bcdf6

--- src/main/java/com/bss/inc/cameraon/utils/$UI$01.java
+++ src/main/java/com/bss/inc/cameraon/utils/$UI$01.java
@@ -80,7 +80,7 @@ public class $UI$01 {
             fpsText.setText(String.valueOf(fps));
         });
 
-        int[] cbt = 
+        int[] cbt = new int[]{};
 
         cbs[0].setOnAction(e -> {});
         cbs[1].setOnAction(e -> {});
