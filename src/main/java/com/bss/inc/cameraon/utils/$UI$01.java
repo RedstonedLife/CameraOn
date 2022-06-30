b85a254410651d902152dc67160d20d432e1e848
Tal A. Baskin <talbaskin.business@gmail.com>
2022-06-30 20:19:33 +0300

    Update-b37e5d82-227b-48d2-9ea3-6f61c546a133

--- src/main/java/com/bss/inc/cameraon/utils/$UI$01.java
+++ src/main/java/com/bss/inc/cameraon/utils/$UI$01.java
@@ -35,6 +35,8 @@ public class $UI$01 {
     }
 
     public static <T> void u_07$1$1(Button[] buttons, Text fpsText, ChoiceBox<T> cbs) {
-        buttons
+        /*
+        Buttons Map (4 Buttons)
+         */
     }
 }
