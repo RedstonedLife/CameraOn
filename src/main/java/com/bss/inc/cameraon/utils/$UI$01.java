c90ed83e8135dc5589d9987d484ef281076253fe
Tal A. Baskin <talbaskin.business@gmail.com>
2022-06-30 20:27:37 +0300

    Update-6b39207d-54c2-4498-904b-e839464cb586

--- src/main/java/com/bss/inc/cameraon/utils/$UI$01.java
+++ src/main/java/com/bss/inc/cameraon/utils/$UI$01.java
@@ -52,7 +52,7 @@ public class $UI$01 {
                 _j.put("frameRate",Integer.valueOf(fpsText.getText())-5);
                 Launcher.SettingsContainer.setValue("cameraSettings",_j);
             }
-            fpsText.getText(String.valueOf(_j.getInt("frameRate"));
+            fpsText.getText(String.valueOf(_j.getInt("frameRate")));
         });
         buttons[1].setOnAction(e -> {
 
