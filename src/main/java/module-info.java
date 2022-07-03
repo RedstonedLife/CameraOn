34e3d35cfb32f866b51034d04876cb769d62f6fb
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-03 17:16:42 +0300

    Update-0d3a080d-5080-4fa9-8a60-f04cff7cf442

---
+++ src/main/java/module-info.java
@@ -0,0 +1,28 @@
+module com.redsoftware.ltd.bnc {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+    requires javafx.media;
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires org.jetbrains.annotations;
+    requires java.base;
+    requires org.json;
+    requires sdp.xsd;
+    requires cxf.services.wsn.api;
+    requires java.xml.bind;
+    requires java.xml.soap;
+    requires commons.codec;
+    requires com.github.oshi;
+    requires com.sun.jna;
+    requires java.prefs;
+    opens com.redsoftware.ltd.bnc to javafx.fxml;
+    opens com.redsoftware.ltd.bnc.controllers to javafx.fxml;
+    opens com.redsoftware.ltd.bnc.utils to javafx.fxml;
+    exports com.bss.inc.cameraon;
+    exports com.bss.inc.cameraon.utils.net.angryscan;
+}
