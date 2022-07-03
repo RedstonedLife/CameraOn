2e2a13da130c59c8e2af9b1dde4b23f4a1d9f1ae
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-03 16:51:09 +0300

    Update-0af60410-8088-4460-ab8b-383c68a84f96

--- src/main/java/com/bss/inc/cameraon/utils/net/angryscan/fetchers/PortsFetcher.java
+++ src/main/java/com/bss/inc/cameraon/utils/net/angryscan/fetchers/PortsFetcher.java
@@ -1,5 +1,6 @@
 package com.bss.inc.cameraon.utils.net.angryscan.fetchers;
 
+import com.bss.inc.cameraon.utils.net.angryscan.config.ScannerConfig;
 import com.redsoftware.ltd.bnc.utils.networking.angryscan.ScanningResult;
 import com.redsoftware.ltd.bnc.utils.networking.angryscan.ScanningSubject;
 import com.redsoftware.ltd.bnc.utils.networking.angryscan.config.ScannerConfig;
