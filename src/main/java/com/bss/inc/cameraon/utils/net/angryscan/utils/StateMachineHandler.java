c4dbc7aea19ffc5bd8845adf48dfdd88179e32cf
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-03 16:53:37 +0300

    Update-112bef3b-ddd7-464a-8438-5a96a4cc1e6e

--- src/main/java/com/bss/inc/cameraon/utils/net/angryscan/utils/StateMachineHandler.java
+++
@@ -1,9 +0,0 @@
-package com.bss.inc.cameraon.utils.net.angryscan.utils;
-
-import com.redsoftware.ltd.bnc.utils.networking.angryscan.net.state.StateMachine;
-
-public class StateMachineHandler {
-
-    private StateMachine stateMachine;
-
-}
