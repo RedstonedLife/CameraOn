33037bdff88c6becd6aa0b3d0db1cd518859735e
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-03 17:41:00 +0300

    Update-ed2b37d0-0903-4175-a8d3-dc7047226bdc

--- src/main/java/com/bss/inc/cameraon/utils/net/angryscan/state/UDPPinger.java
+++ src/main/java/com/bss/inc/cameraon/utils/net/angryscan/state/UDPPinger.java
@@ -1,5 +1,7 @@
 package com.bss.inc.cameraon.utils.net.angryscan.state;
 
+import com.bss.inc.cameraon.logging.Logger;
+
 import java.io.IOException;
 import java.net.*;
 import java.nio.ByteBuffer;
