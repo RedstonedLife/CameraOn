2cda7387e686487b1ccabacd47609b95a3b98b94
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-03 17:06:47 +0300

    Update-d950d755-8abe-4dc5-87f1-d75d7ff1cc37

--- src/main/java/com/bss/inc/cameraon/utils/net/angryscan/core/ScannerDispatcherThread.java
+++ src/main/java/com/bss/inc/cameraon/utils/net/angryscan/core/ScannerDispatcherThread.java
@@ -7,6 +7,7 @@ import com.bss.inc.cameraon.utils.net.angryscan.ScanningResultList;
 import com.bss.inc.cameraon.utils.net.angryscan.ScanningSubject;
 import com.bss.inc.cameraon.utils.net.angryscan.config.ScannerConfig;
 import com.bss.inc.cameraon.utils.net.angryscan.feeders.Feeder;
+import com.bss.inc.cameraon.utils.net.angryscan.state.state.ScanningState;
 import com.bss.inc.cameraon.utils.net.angryscan.state.state.StateMachine;
 import com.bss.inc.cameraon.utils.net.angryscan.state.state.StateTransitionListener;
 
