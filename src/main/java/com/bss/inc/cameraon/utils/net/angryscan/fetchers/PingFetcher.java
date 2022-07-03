9109dc655380af79d87d21559900deb9e22f5c95
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-03 16:50:18 +0300

    Update-8b417c5d-edfb-4875-8906-3851e65a3bf6

--- src/main/java/com/bss/inc/cameraon/utils/net/angryscan/fetchers/PingFetcher.java
+++ src/main/java/com/bss/inc/cameraon/utils/net/angryscan/fetchers/PingFetcher.java
@@ -1,5 +1,6 @@
 package com.bss.inc.cameraon.utils.net.angryscan.fetchers;
 
+import com.bss.inc.cameraon.logging.Logger;
 import com.redsoftware.ltd.bnc.utils.logging.Logger;
 import com.redsoftware.ltd.bnc.utils.networking.angryscan.ScanningResult.ResultType;
 import com.redsoftware.ltd.bnc.utils.networking.angryscan.ScanningSubject;
