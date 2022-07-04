f1b32976140241b201783ceee80ba44945418a2e
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-04 17:10:09 +0300

    Update-7ec499e4-6453-49c6-99d8-085eaf4fe863

--- src/main/java/com/bss/inc/cameraon/Launcher.java
+++ src/main/java/com/bss/inc/cameraon/Launcher.java
@@ -36,7 +36,7 @@ public class Launcher {
         SettingsContainer.setValue("cameraSettings",j_);
         j_ = (JSONObject) SettingsContainer.getValue("scanSettings");
         if(!j_.has("")) {} // Thread Delay threadDelay (20 MS Default)
-        if(!j_.has("")) {} // Max Threads (255 Defaults
+        if(!j_.has("")) {} // Max Threads (255 Default)
         if(!j_.has("")) {} // Thread Delay threadDelay (20 MS Default)
         if(!j_.has("")) {} // Thread Delay threadDelay (20 MS Default)
         if(!j_.has("")) {} // Thread Delay threadDelay (20 MS Default)
