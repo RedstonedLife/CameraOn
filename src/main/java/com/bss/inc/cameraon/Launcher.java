f626a6d31a84dc1451728b3bdeb0c7e2c5ac0df6
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-04 17:09:00 +0300

    Update-9d8b5903-6b2f-433f-81ef-6796c862110b

--- src/main/java/com/bss/inc/cameraon/Launcher.java
+++ src/main/java/com/bss/inc/cameraon/Launcher.java
@@ -34,7 +34,8 @@ public class Launcher {
         if(!j_.has("bitrate")) {j_.put("bitrate",0);}
         if(!j_.has("videnc")) {j_.put("videnc",0);}
         SettingsContainer.setValue("cameraSettings",j_);
-        JSONObject j_ = (JSONObject) SettingsContainer.getValue("scanSettings");
+        j_ = (JSONObject) SettingsContainer.getValue("scanSettings");
+        if(!j_.has("")) {}
         SettingsContainer.Save();
     }
 
