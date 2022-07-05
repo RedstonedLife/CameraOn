8d7560457678212529a17bf93bcf8b235cae278e
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-05 19:02:50 +0300

    Update-bb556f3f-83b9-4422-a1c3-025497a1a0fd

--- src/main/java/com/bss/inc/cameraon/Launcher.java
+++ src/main/java/com/bss/inc/cameraon/Launcher.java
@@ -77,7 +77,8 @@ public class Launcher {
     }
 
     public static void main(String[] args) throws UnknownOS, FileNotFoundException {
-        $UI$01.u_08$1$4("80,88,80-500");
+        System.out.println($UI$01.u_08$1$4("80,88,80-500"));
+        System.out.println($UI$01.u_08$1$4("80,88,80-500d"));
         //setSettings();
         //MainClass.main(args);
     }
