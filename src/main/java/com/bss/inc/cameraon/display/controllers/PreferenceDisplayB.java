e6563b9f068cc1f6a10766dc19f711bb3d337111
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-05 17:56:34 +0300

    Update-1f93f0ac-1485-4190-ad5a-30f72c3b6cdd

--- src/main/java/com/bss/inc/cameraon/display/controllers/PreferenceDisplayB.java
+++ src/main/java/com/bss/inc/cameraon/display/controllers/PreferenceDisplayB.java
@@ -32,7 +32,7 @@ public class PreferenceDisplayB implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        $UI$01.u_08$1$2(new TextField[]{threadsDelayTf,threadsMaxTf,pingProbesTf,pingTimeoutTf,defaultPortTimeoutTf,minAdptdConTimeoutTf},new CheckBox[]{scanDeadHostsCHKb,skipUnassignedCHKb,adaptTimeoutToPingCHKb});
+        $UI$01.u_08$1$2(new TextField[]{threadsDelayTf,threadsMaxTf,pingProbesTf,pingTimeoutTf,defaultPortTimeoutTf,minAdptdConTimeoutTf},new CheckBox[]{scanDeadHostsCHKb,skipUnassignedCHKb,adaptTimeoutToPingCHKb,});
         asSceneBtn.setOnAction(e -> {MainClass.getWindowManager().changeDialogScene("preferencesDialog",MainClass.class.getResource(FrontendPaths.PREF_DISPLAY_AS_FXML));});
         displaySceneBtn.setOnAction(e -> {MainClass.getWindowManager().changeDialogScene("preferencesDialog",MainClass.class.getResource(FrontendPaths.PREF_DISPLAY_FXML));});
         initializeLanguage();
