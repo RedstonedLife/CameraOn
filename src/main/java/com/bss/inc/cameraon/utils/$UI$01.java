d8f44ae1632b6ef839ed6c98c953e105d4f16d18
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-05 19:28:18 +0300

    Update-b7e7b68f-fc18-4a11-9de4-aa029a94baf1

--- src/main/java/com/bss/inc/cameraon/utils/$UI$01.java
+++ src/main/java/com/bss/inc/cameraon/utils/$UI$01.java
@@ -156,6 +156,7 @@ public class $UI$01 {
         Text Area ptsct - portsToScan (portString)
         PingMethod<String> - selectedPinger
          */
+        
     }
 
     public static void u_08$1$1(ScannerConfig scannerConfig, TextField[] tfs, CheckBox[] chkbs, TextArea ptsct, ChoiceBox<String> pingMethod) {
