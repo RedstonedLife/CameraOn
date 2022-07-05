b8e594461545b479b9767cc9c0b855eca12ac89a
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-05 17:19:21 +0300

    Update-ddcc0d6f-1f33-49fa-aae1-2ed22deb666e

--- src/main/java/com/bss/inc/cameraon/display/controllers/PreferenceDisplayB.java
+++ src/main/java/com/bss/inc/cameraon/display/controllers/PreferenceDisplayB.java
@@ -23,7 +23,7 @@ public class PreferenceDisplayB implements Initializable {
 
     @FXML private TextField threadsDelayTf,threadsMaxTf,defaultPortTimeoutTf,minAdptdConTimeoutTf,pingProbesTf,pingTimeoutTf;
     @FXML private TextArea portToScanTa;
-    @FXML private ChoiceBox<String> pingMethodCb
+    @FXML private ChoiceBox<String> pingMethodCb;
 
     private void initializeLanguage() {
         displaySceneBtn.setText(tl("preferences.btn.display.text"));
