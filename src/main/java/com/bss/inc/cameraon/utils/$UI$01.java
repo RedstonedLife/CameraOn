2b938d18943fc49f0467712d4cf4d4a9e69f223d
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-05 18:39:28 +0300

    Update-e793d91f-00a5-4df0-abb7-2903466ae01e

--- src/main/java/com/bss/inc/cameraon/utils/$UI$01.java
+++ src/main/java/com/bss/inc/cameraon/utils/$UI$01.java
@@ -15,9 +15,10 @@ import org.json.JSONObject;
 
 import java.io.FileNotFoundException;
 import java.util.Map;
+import java.util.regex.Pattern;
 
 public class $UI$01 {
-    public static final String num_regex = "[0-9]+";
+    public static final Pattern digitPattern = Pattern.compile("[]")
     public static void u_00$1$1(SplitPane csplitpane) {csplitpane.getDividers().get(0).positionProperty().addListener((observableValue, number, t1) -> csplitpane.getDividers().get(0).setPosition(FrontendPaths.DIVID_POS));}
     public static void u_01$1$1(SplitPane csplitpane, Button[] buttons) {
         u_00$1$1(csplitpane);
