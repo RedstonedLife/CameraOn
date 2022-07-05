bb1b37ad6db7fc227a0190d88c13d6a5ca0a16ad
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-05 18:48:33 +0300

    Update-d78d019a-0974-4e19-8a77-58dcecabb4eb

--- src/main/java/com/bss/inc/cameraon/utils/$UI$01.java
+++ src/main/java/com/bss/inc/cameraon/utils/$UI$01.java
@@ -134,9 +134,11 @@ public class $UI$01 {
         Text Area ptsct - portsToScan (portString)
         PingMethod<String> - selectedPinger
          */
-        for (:
-             ) {
-            
+        for (TextField textField: tfs) {
+            textField.textProperty().addListener((observable, oldValue, newValue) -> {
+                if(oldValue == null || oldValue == "") {return;}
+                if(!u_08$1$3(newValue)) {tfs[0].setText(oldValue);}
+            });
         }
 
     }
