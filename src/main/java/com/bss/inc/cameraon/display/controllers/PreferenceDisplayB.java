c03e95a7dcc85e45b324d8a967b3289a7a4d563e
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-05 17:55:44 +0300

    Update-8b9362de-4f65-4e63-b57c-05261558fd51

--- src/main/java/com/bss/inc/cameraon/display/controllers/PreferenceDisplayB.java
+++ src/main/java/com/bss/inc/cameraon/display/controllers/PreferenceDisplayB.java
@@ -32,7 +32,7 @@ public class PreferenceDisplayB implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        $UI$01.u_08$1$2(new TextField[]{threadsDelayTf,});
+        $UI$01.u_08$1$2(new TextField[]{threadsDelayTf,maxT});
         asSceneBtn.setOnAction(e -> {MainClass.getWindowManager().changeDialogScene("preferencesDialog",MainClass.class.getResource(FrontendPaths.PREF_DISPLAY_AS_FXML));});
         displaySceneBtn.setOnAction(e -> {MainClass.getWindowManager().changeDialogScene("preferencesDialog",MainClass.class.getResource(FrontendPaths.PREF_DISPLAY_FXML));});
         initializeLanguage();
