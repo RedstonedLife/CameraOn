9ec67904407343eb87c7cc45959fe49f5543643a
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-05 19:31:52 +0300

    Update-fa52f277-9a6c-4bc1-b4c6-5eb4e62907e9

--- src/main/java/com/bss/inc/cameraon/utils/$UI$01.java
+++ src/main/java/com/bss/inc/cameraon/utils/$UI$01.java
@@ -167,6 +167,8 @@ public class $UI$01 {
         scannerConfig.skipBroadcastAddresses = chkbs[1].isSelected();
         scannerConfig.adaptPortTimeout = chkbs[2].isSelected();
         scannerConfig.useRequestedPorts = chkbs[3].isSelected();
+        scannerConfig.portString = ptsct.getText();
+        
     }
 
     public static void u_08$1$1(ScannerConfig scannerConfig, TextField[] tfs, CheckBox[] chkbs, TextArea ptsct, ChoiceBox<String> pingMethod) {
