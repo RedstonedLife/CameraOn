f3214019be510c75c47bf77eca9d11a295889d36
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-05 18:48:00 +0300

    Update-fd42c9a2-138a-405e-8810-f19a9f31c699

--- src/main/java/com/bss/inc/cameraon/utils/$UI$01.java
+++ src/main/java/com/bss/inc/cameraon/utils/$UI$01.java
@@ -134,7 +134,7 @@ public class $UI$01 {
         Text Area ptsct - portsToScan (portString)
         PingMethod<String> - selectedPinger
          */
-        tfs[].textProperty().addListener((observable, oldValue, newValue) -> {
+        tfs[0].textProperty().addListener((observable, oldValue, newValue) -> {
             if(oldValue == null || oldValue == "") {return;}
             if(!u_08$1$3(newValue)) {tfs[0].setText(oldValue);}
         });
