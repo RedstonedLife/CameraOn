ab4fff35b536f7fcf47ac9e13bd0b0dc44ded0a5
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-05 18:33:22 +0300

    Update-1e062dd8-d5a8-4c2c-82a1-6755816f6a42

--- src/main/java/com/bss/inc/cameraon/display/controllers/PreferenceDisplayB.java
+++ src/main/java/com/bss/inc/cameraon/display/controllers/PreferenceDisplayB.java
@@ -31,6 +31,8 @@ public class PreferenceDisplayB implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         pingMethodCb.getItems().addAll("pinger.windows", "pinger.udp", "pinger.tcp", "pinger.combined", "pinger.java", "pinger.arp");
+        $UI$01.u_08$1$1(new TextField[]{threadsDelayTf,threadsMaxTf,pingProbesTf,pingTimeoutTf,defaultPortTimeoutTf,minAdptdConTimeoutTf},
+                new CheckBox[]{scanDeadHostsCHKb,skipUnassignedCHKb,adaptTimeoutToPingCHKb,forEachHostCHKb},portToScanTa,pingMethodCb);
         $UI$01.u_08$1$2(new TextField[]{threadsDelayTf,threadsMaxTf,pingProbesTf,pingTimeoutTf,defaultPortTimeoutTf,minAdptdConTimeoutTf},
                 new CheckBox[]{scanDeadHostsCHKb,skipUnassignedCHKb,adaptTimeoutToPingCHKb,forEachHostCHKb},portToScanTa,pingMethodCb);
         asSceneBtn.setOnAction(e -> {MainClass.getWindowManager().changeDialogScene("preferencesDialog",MainClass.class.getResource(FrontendPaths.PREF_DISPLAY_AS_FXML));});
