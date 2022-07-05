e6d8b3d03ced65c12c1a6df74d2fa4e20ffac237
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-05 18:06:41 +0300

    Update-df10cd08-be04-4f94-897c-8cf1e0f31480

--- src/main/java/com/bss/inc/cameraon/display/controllers/PreferenceDisplayB.java
+++ src/main/java/com/bss/inc/cameraon/display/controllers/PreferenceDisplayB.java
@@ -30,7 +30,7 @@ public class PreferenceDisplayB implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        u_05$1$1()
+        $UI$01.u_05$1$1()
         $UI$01.u_08$1$2(new TextField[]{threadsDelayTf,threadsMaxTf,pingProbesTf,pingTimeoutTf,defaultPortTimeoutTf,minAdptdConTimeoutTf},
                 new CheckBox[]{scanDeadHostsCHKb,skipUnassignedCHKb,adaptTimeoutToPingCHKb,forEachHostCHKb},portToScanTa,pingMethodCb);
         asSceneBtn.setOnAction(e -> {MainClass.getWindowManager().changeDialogScene("preferencesDialog",MainClass.class.getResource(FrontendPaths.PREF_DISPLAY_AS_FXML));});
