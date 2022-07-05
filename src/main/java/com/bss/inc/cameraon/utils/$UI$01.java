d3cc48948b3f47b82fe120c34a5e39c86e2dfd08
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-05 19:30:32 +0300

    Update-b2839f95-8b1a-498d-b04b-185401b35dfa

--- src/main/java/com/bss/inc/cameraon/utils/$UI$01.java
+++ src/main/java/com/bss/inc/cameraon/utils/$UI$01.java
@@ -163,7 +163,7 @@ public class $UI$01 {
         scannerConfig.pingTimeout = Integer.valueOf(tfs[3].getText());
         scannerConfig.portTimeout = Integer.valueOf(tfs[4].getText());
         scannerConfig.minPortTimeout = Integer.valueOf(tfs[5].getText());
-        
+        chkbs
     }
 
     public static void u_08$1$1(ScannerConfig scannerConfig, TextField[] tfs, CheckBox[] chkbs, TextArea ptsct, ChoiceBox<String> pingMethod) {
