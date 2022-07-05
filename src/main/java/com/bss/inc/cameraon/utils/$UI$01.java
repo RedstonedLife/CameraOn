4cde3ab9109fcdd0c518de80701f01ecd7238844
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-05 19:27:56 +0300

    Update-f3125b31-14a8-4696-b8fd-2500bb1d45af

--- src/main/java/com/bss/inc/cameraon/utils/$UI$01.java
+++ src/main/java/com/bss/inc/cameraon/utils/$UI$01.java
@@ -139,6 +139,7 @@ public class $UI$01 {
                     Character.isWhitespace(c) && pc == ',';
 
     }
+    public static void u_08$
 
     public static void u_08$1$1(ScannerConfig scannerConfig, TextField[] tfs, CheckBox[] chkbs, TextArea ptsct, ChoiceBox<String> pingMethod) {
         /*
