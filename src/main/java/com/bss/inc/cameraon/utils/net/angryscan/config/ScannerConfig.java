1f9bea821cbca02090a2e26d44cfbb683209cdce
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-06 19:08:16 +0300

    Update-1ca70af6-d766-4636-bae5-88456b216c98

--- src/main/java/com/bss/inc/cameraon/utils/net/angryscan/config/ScannerConfig.java
+++ src/main/java/com/bss/inc/cameraon/utils/net/angryscan/config/ScannerConfig.java
@@ -64,8 +64,8 @@ public class ScannerConfig {
         minPortTimeout = useDefault ? (int) data[9] : preferences.getInt("minPortTimeout", 100);
         portString = useDefault ? (String) data[10] : preferences.get("portString", "80,443,8080");
         useRequestedPorts = useDefault ? (boolean) data[11] : preferences.getBoolean("useRequestedPorts", true);
-        notAvailableText = useDefault ? (String) data[12] : preferences.get("notAvailableText", I18n.tl("fetcher.value.notAvailable"));
-        notScannedText = useDefault ? (String) data[13] : preferences.get("notScannedText", I18n.tl("fetcher.value.notScanned"));
+        notAvailableText = useDefault ? (String) data[12] : preferences.get("notAvailableText", "[n/a]");
+        notScannedText = useDefault ? (String) data[13] : preferences.get("notScannedText", "[n/s]");
         displayResultType = useDefault ? (DisplayResultType) data[14] : DisplayResultType.fromString(preferences.get("displayResult", "ALL"));
         askConfirmationBeforeScan = useDefault ? (boolean) data[15] : preferences.getBoolean("askConfirmationBeforeScan", false);
         showInfoDialogAfterScan = useDefault ? (boolean) data[16] : preferences.getBoolean("showInfoDialogAfterScan", false);
