2dab21803cf552fe87f6327cbe1e021c651e7042
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-06 18:54:50 +0300

    Update-ea13aa2a-14ce-4d02-a387-0641ebc41c91

--- src/main/java/com/bss/inc/cameraon/utils/$UI$01.java
+++ src/main/java/com/bss/inc/cameraon/utils/$UI$01.java
@@ -253,7 +253,7 @@ public class $UI$01 {
     public static void u_09$1$1(TextField[] tfs, CheckBox[] chkbs, RadioButton[] rbs) {
         /*
         TextField Array Layout
-        0 - NotAvailable Text Field         naTf
+        0 - NotAvailable Text Field                  naTf
         1 - NotScanned Text Field                    nsTf
         CheckBox Array Layout
         0 - Ask Confirmation Before Scan             askConfirmBeforeScan
