6e1cbe64d4cd8ba8a04ac2361eb0ad7ae4dc1b96
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-06 19:05:52 +0300

    Update-c2b0df25-85f3-46d3-a5fa-bb3861d85d8e

--- src/main/java/com/bss/inc/cameraon/utils/net/angryscan/config/ScannerConfig.java
+++ src/main/java/com/bss/inc/cameraon/utils/net/angryscan/config/ScannerConfig.java
@@ -66,7 +66,8 @@ public class ScannerConfig {
         useRequestedPorts = useDefault ? (boolean) data[11] : preferences.getBoolean("useRequestedPorts", true);
         notAvailableText = useDefault ? (String) data[12] : preferences.get("notAvailableText", I18n.tl("fetcher.value.notAvailable"));
         notScannedText = useDefault ? (String) data[13] : preferences.get("notScannedText", I18n.tl("fetcher.value.notScanned"));
-        displayResultType = useDefault ? (DisplayResultType) data[14] : DisplayResultType.fromString(preferences.get("displayResult", "ALL"))
+        displayResultType = useDefault ? (DisplayResultType) data[14] : DisplayResultType.fromString(preferences.get("displayResult", "ALL"));
+        
     }
 
     /**
