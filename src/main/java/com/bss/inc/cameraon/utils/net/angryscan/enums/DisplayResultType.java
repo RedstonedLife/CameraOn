76658f9c2dc7776727e1c6618d422b9f5ace84cd
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-06 18:56:21 +0300

    Update-16ea9133-d993-4232-8c00-563224223f3c

--- src/main/java/com/bss/inc/cameraon/utils/net/angryscan/enums/DisplayResultType.java
+++ src/main/java/com/bss/inc/cameraon/utils/net/angryscan/enums/DisplayResultType.java
@@ -2,4 +2,9 @@ package com.bss.inc.cameraon.utils.net.angryscan.enums;
 
 public enum DisplayResultType {
     ALL,
+    ALIVE,
+    OPEN_PORTS_ONLY;
+
+    private String string;
+    DisplayResultType()
 }
