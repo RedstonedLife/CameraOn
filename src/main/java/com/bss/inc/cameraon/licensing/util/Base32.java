28590b6484ee95fb6ec79ae0095dd5aa45b68206
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-06 22:11:45 +0300

    Update-c39f866a-832b-460f-9ca1-dd8110ab3472

--- src/main/java/com/bss/inc/cameraon/licensing/util/Base32.java
+++ src/main/java/com/bss/inc/cameraon/licensing/util/Base32.java
@@ -16,6 +16,41 @@ public class Base32 {
             case 3 -> j = 3;
             case 4 -> j = 1;
         }
-        
+        StringBuffer stringBuffer = new StringBuffer((bytes.length + 7 << 3) / 5 + j);
+        while (b < bytes.length) {
+            j = (bytes[b] >= 0) ? bytes[b] : (bytes[b] + 256);
+            if (i > 3) {
+                boolean bool;
+                if (b + 1 < bytes.length) {
+                    bool = (bytes[b + 1] >= 0) ? bytes[b + 1] : (bytes[b + 1] + 256);
+                } else {
+                    bool = false;
+                }
+                j &= 255 >> i;
+                i = (i + 5) % 8;
+                j = j << i | bool >> 8 - i;
+                b++;
+            } else {
+                j = j >> 8 - i + 5 & 0x1F;
+                if ((i = (i + 5) % 8) == 0)
+                    b++;
+            }
+            stringBuffer.append("ABCDEFGHIJKLMNOPQRSTUVWXYZ234567".charAt(j));
+        }
+        switch (bytes.length) {
+            case 1:
+                stringBuffer.append("======");
+                break;
+            case 2:
+                stringBuffer.append("====");
+                break;
+            case 3:
+                stringBuffer.append("===");
+                break;
+            case 4:
+                stringBuffer.append("=");
+                break;
+        }
+        return stringBuffer.toString();
     }
 }
