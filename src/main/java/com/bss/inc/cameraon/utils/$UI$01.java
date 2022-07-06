8c34370d4a6b9e7832a0c32b3f82077025adbc0b
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-06 18:54:36 +0300

    Update-6da45e52-cc1d-4483-95a8-be812c5c63b1

--- src/main/java/com/bss/inc/cameraon/utils/$UI$01.java
+++ src/main/java/com/bss/inc/cameraon/utils/$UI$01.java
@@ -259,7 +259,7 @@ public class $UI$01 {
         0 - Ask Confirmation Before Scan    askConfirmBeforeScan
         1 - Show Info Dialog After Scan     showInfoAfterScan
         RadioButton Array Layout
-        0 - All Scanned Hosts -             DISPLAY ALL
+        0 - All Scanned Hosts -                      DISPLAY ALL
         1 - Alive Hosts (Responding to Pings) only - ALIVE
         2 - Hosts with open ports only
          */
