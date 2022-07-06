af8b7e0ad12c5a5e6b62ee7f1ab096c122a99b4d
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-06 22:33:02 +0300

    Update-6fe4a987-e9b3-40af-b002-24bc2e0460a8

--- src/main/java/com/bss/inc/cameraon/licensing/util/Hex.java
+++ src/main/java/com/bss/inc/cameraon/licensing/util/Hex.java
@@ -1,2 +1,35 @@
-package com.bss.inc.cameraon.licensing.util;public class Hex {
+package com.bss.inc.cameraon.licensing.util;
+
+public class Hex {
+    private static final char[] a = new char[] {
+            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+            'a', 'b', 'c', 'd', 'e', 'f' };
+
+    public static byte[] decodeHex(char[] data) {
+        int i;
+        byte[] arrayOfByte = new byte[(i = data.length) >> 1];
+        for (byte b1 = 0, b2 = 0; b2 < i; b1++) {
+            int j = toDigit(data[b2], b2) << 4;
+            b2++;
+            j |= toDigit(data[b2], b2);
+            b2++;
+            arrayOfByte[b1] = (byte)j;
+        }
+        return arrayOfByte;
+    }
+
+    public static String encodeHexString(byte[] data) {
+        int i;
+        char[] arrayOfChar = new char[(i = data.length) << 1];
+        for (byte b1 = 0, b2 = 0; b1 < i; b1++) {
+            arrayOfChar[b2++] = a[(0xF0 & data[b1]) >>> 4];
+            arrayOfChar[b2++] = a[0xF & data[b1]];
+        }
+        return new String(arrayOfChar);
+    }
+
+    protected static int toDigit(char ch, int index) {
+        return Character.digit(ch, 16);
+    }
 }
+
