0cad8d50b8e29ad990d7ae4dcff867dbc42eddac
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-06 22:18:07 +0300

    Update-5e328477-b2de-48fb-a80f-7a3f076c78f3

--- src/main/java/com/bss/inc/cameraon/licensing/util/Crypto.java
+++ src/main/java/com/bss/inc/cameraon/licensing/util/Crypto.java
@@ -1,7 +1,11 @@
 package com.bss.inc.cameraon.licensing.util;
 
+import javax.crypto.NoSuchPaddingException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+
 public class Crypto {
     private static String a = null;
 
-    public static String encrypt(String data)
+    public static String encrypt(String data) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeySpecException
 }
