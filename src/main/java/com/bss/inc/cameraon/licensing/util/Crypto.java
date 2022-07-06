1018b61bd7190aeed0f274484ad37a07ce3eab8a
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-06 22:28:04 +0300

    Update-022fc851-ac9e-4456-9cb0-bc27b3e5754f

--- src/main/java/com/bss/inc/cameraon/licensing/util/Crypto.java
+++ src/main/java/com/bss/inc/cameraon/licensing/util/Crypto.java
@@ -86,7 +86,7 @@ public class Crypto {
         try {
             str = new String(cipher.doFinal(Hex.decodeHex(paramString1.toCharArray())), "UTF-8");
         } catch (Exception exception) {
-            Logger.getLogger(Crypto.class.getName()).log(Level.ERROR, "Decryption error.", exception);
+            logger.log(Level.ERROR, "Decryption error.", exception);
         }
         return str;
     }
