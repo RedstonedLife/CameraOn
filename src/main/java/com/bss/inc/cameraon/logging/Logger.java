6e116e2a848f0250537dddaeb921253184afe8c2
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-06 22:29:18 +0300

    Update-86ab197c-fed7-4067-9265-3407e9f7b06c

--- src/main/java/com/bss/inc/cameraon/logging/Logger.java
+++ src/main/java/com/bss/inc/cameraon/logging/Logger.java
@@ -45,4 +45,8 @@ public class Logger implements ErrorHandler, WarnHandler, InfoHandler, DebugHand
     @Override public void warn(Exception e) {LogManager.getLFM().warn(e);}
     @Override public void warn(String message, Exception e, Class<?> clazz) {LogManager.getLFM().warn(message, e, clazz);}
     @Override public void warn(String message, Exception e, String subname) {LogManager.getLFM().warn(message, e, subname);}
+
+    @Override
+    public void log(Logger logger, Level level, String message) {
+    }
 }
