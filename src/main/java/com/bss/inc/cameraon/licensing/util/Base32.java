3bfbcdbdf0394bd44af9a95eb47d026d8e262efd
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-06 22:09:47 +0300

    Update-27428d6a-37fb-43cd-94e8-2eaf64dc5f8a

--- src/main/java/com/bss/inc/cameraon/licensing/util/Base32.java
+++ src/main/java/com/bss/inc/cameraon/licensing/util/Base32.java
@@ -1,4 +1,7 @@
 package com.bss.inc.cameraon.licensing.util;
 
 public class Base32 {
+    private static final int[] a = new int[] {
+            255,255,26,27,28,29,30,31,255,255,255,255,255,25
+    }
 }
