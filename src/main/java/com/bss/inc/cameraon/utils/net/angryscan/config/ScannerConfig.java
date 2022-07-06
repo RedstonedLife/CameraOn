e85510e9da727f30509c67f37b39a28b914f46ae
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-06 19:03:58 +0300

    Update-8b05310e-c350-45ee-8521-fe0472f7fafa

--- src/main/java/com/bss/inc/cameraon/utils/net/angryscan/config/ScannerConfig.java
+++ src/main/java/com/bss/inc/cameraon/utils/net/angryscan/config/ScannerConfig.java
@@ -106,7 +106,7 @@ public class ScannerConfig {
                 j_.getBoolean("useRequestedPorts"),
                 rj_.getString("valueNotAvailable"),
                 rj_.getString("valueNotScanned"),
-                DisplayResultType
+                DisplayResultType.fromString(rj_.getString(""))
         };
     }
 
