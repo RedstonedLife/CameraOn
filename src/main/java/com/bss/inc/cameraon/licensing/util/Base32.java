51a1f1a240a332d7d0225ebd79e62f1f067f90dc
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-06 22:11:04 +0300

    Update-df6806c6-a1d2-47f0-96e8-cd330a0aa94c

--- src/main/java/com/bss/inc/cameraon/licensing/util/Base32.java
+++ src/main/java/com/bss/inc/cameraon/licensing/util/Base32.java
@@ -7,6 +7,12 @@ public class Base32 {
             3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,255,255,255,255,255};
 
     public static String encode(byte[] bytes) {
-        
+        byte b = 0;
+        int i = 0;
+        int j = 0;
+        switch(bytes.length) {
+            case 1:
+                
+        }
     }
 }
