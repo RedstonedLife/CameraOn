db26f33ddd0f81d2f2b2cad695aaf94cc184f315
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-06 19:52:30 +0300

    Update-27bfd4f9-ee26-48cb-8f25-ec306af0a74f

--- src/main/java/com/bss/inc/cameraon/utils/$UI$01.java
+++ src/main/java/com/bss/inc/cameraon/utils/$UI$01.java
@@ -16,7 +16,7 @@ import java.util.regex.Pattern;
 
 public class $UI$01 {
     public static final Pattern digitPattern;
-    static TextField[] tfs;
+    static TextField[] tfs,tfs;
     static CheckBox[] chkbs;
     static TextArea ptsct;
     static ChoiceBox<String> pingMethod;
