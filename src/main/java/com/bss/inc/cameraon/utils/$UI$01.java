32cbf1a9e50eaafe467a3579851b82d09aa20ddf
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-06 21:02:51 +0300

    Update-2114651e-3795-4470-958b-f7de5a1ef2b4

--- src/main/java/com/bss/inc/cameraon/utils/$UI$01.java
+++ src/main/java/com/bss/inc/cameraon/utils/$UI$01.java
@@ -288,6 +288,7 @@ public class $UI$01 {
         if(j_.has("valueNotScanned")) {tfs2[1].setText(j_.getString("valueNotScanned"));}
         if(j_.has("askConfirmationBeforeScan")) {chkbs2[0].setSelected(j_.getBoolean("askConfirmationBeforeScan"));}
         if(j_.has("showInfoDialogAfterScan")) {chkbs2[1].setSelected(j_.getBoolean("showInfoDialogAfterScan"));}
+        System.out.println("NA: " + j_.getString("valueNotAvailable"));
         System.out.println("NS: " + j_.getString("valueNotScanned"));
     }
 
