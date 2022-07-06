d9016bd80a3b76ac2229a6ab5dbce5dbce7fb251
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-06 18:15:23 +0300

    Update-6f55a318-0be9-465a-95d4-2831f49550d6

--- src/main/java/com/bss/inc/cameraon/utils/net/angryscan/config/ScannerConfig.java
+++ src/main/java/com/bss/inc/cameraon/utils/net/angryscan/config/ScannerConfig.java
@@ -48,8 +48,8 @@ public class ScannerConfig {
 
         maxThreads = useDefault ? (int) data[0] : preferences.getInt("maxThreads", Platform.CRIPPLED_WINDOWS ? 10 : 255);
         threadDelay = useDefault ? (int) data[1] : preferences.getInt("threadDelay", 20);
-        scanDeadHosts = useDefault ? (int) data[0] : preferences.getBoolean("scanDeadHosts", true);
-        selectedPinger = useDefault ? (int) data[0] : preferences.get("selectedPinger", Platform.WINDOWS ? "pinger.windows" : "pinger.arp");
+        scanDeadHosts = useDefault ? (boolean) data[2] : preferences.getBoolean("scanDeadHosts", true);
+        selectedPinger = useDefault ? (String) data[3] : preferences.get("selectedPinger", Platform.WINDOWS ? "pinger.windows" : "pinger.arp");
         pingTimeout = useDefault ? (int) data[0] : preferences.getInt("pingTimeout", 2000);
         pingCount = useDefault ? (int) data[0] : preferences.getInt("pingCount", 3);
         skipBroadcastAddresses = useDefault ? (int) data[0] : preferences.getBoolean("skipBroadcastAddress", true);
