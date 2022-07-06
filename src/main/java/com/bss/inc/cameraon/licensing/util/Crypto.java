a6d37af8003dd8c7aa9f83b8348b60759d5c7070
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-06 22:19:45 +0300

    Update-a4a899aa-d2ff-47f7-886a-9caa18c929c2

--- src/main/java/com/bss/inc/cameraon/licensing/util/Crypto.java
+++ src/main/java/com/bss/inc/cameraon/licensing/util/Crypto.java
@@ -1,8 +1,10 @@
 package com.bss.inc.cameraon.licensing.util;
 
-import javax.crypto.BadPaddingException;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
+import org.apache.commons.codec.binary.Hex;
+
+import javax.crypto.*;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.PBEParameterSpec;
 import java.io.UnsupportedEncodingException;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
@@ -25,5 +27,28 @@ public class Crypto {
         return a(data, a);
     }
 
-    
+    private static String a(String paramString1, String paramString2) throws NoSuchAlgorithmException, NoSuchPaddingException,
+            InvalidKeySpecException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException,
+            InvalidAlgorithmParameterException, UnsupportedEncodingException {
+        byte[] arrayOfByte = { -57, 115, 33, -116, 126, -56, -18, -103 };
+        PBEParameterSpec pBEParameterSpec = new PBEParameterSpec(arrayOfByte, 10);
+        PBEKeySpec pBEKeySpec = new PBEKeySpec(paramString2.toCharArray());
+        SecretKey secretKey = SecretKeyFactory.getInstance(new String(Base32.decode("KBBEKV3JORUE2RBVIFXGIRCFKM"))).generateSecret(pBEKeySpec);
+        Cipher cipher;
+        (cipher = Cipher.getInstance(new String(Base32.decode("KBBEKV3JORUE2RBVIFXGIRCFKM")))).init(1, secretKey, pBEParameterSpec);
+        paramString1 = Hex.encodeHexString(cipher.doFinal(paramString1.getBytes("UTF-8")));
+        String str = "";
+        if (paramString1.length() > 60) {
+            byte b = 0;
+            while (b <= paramString1.length() - 60) {
+                str = str + paramString1.substring(b, b + 60) + "\n";
+                b += 60;
+            }
+            if (paramString1.length() > b - 60)
+                str = str + paramString1.substring(b, paramString1.length()) + "\n";
+        } else {
+            str = paramString1;
+        }
+        return str;
+    }
 }
