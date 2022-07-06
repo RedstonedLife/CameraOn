30cf9ceff3b0bce56d8e73e5a1f26a1dec60206b
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-06 22:32:44 +0300

    Update-cece31fa-8449-4280-8792-ca9ca2686cd7

--- src/main/java/com/bss/inc/cameraon/licensing/util/FileUtils.java
+++ src/main/java/com/bss/inc/cameraon/licensing/util/FileUtils.java
@@ -1,4 +1,26 @@
 package com.bss.inc.cameraon.licensing.util;
 
 public class FileUtils {
+    public static String readFile(String fileName) throws IOException {
+        FileReader fileReader = new FileReader(fileName);
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        StringBuilder stringBuilder = new StringBuilder();
+        String str;
+        while ((str = bufferedReader.readLine()) != null) {
+            stringBuilder.append(str);
+            stringBuilder.append("\n");
+        }
+        bufferedReader.close();
+        fileReader.close();
+        return stringBuilder.toString();
+    }
+
+    public static boolean writeFile(String fileName, String data) throws IOException {
+        FileWriter fileWriter = new FileWriter(fileName);
+        BufferedWriter bufferedWriter;
+        (bufferedWriter = new BufferedWriter(fileWriter)).write(data);
+        bufferedWriter.close();
+        fileWriter.close();
+        return true;
+    }
 }
