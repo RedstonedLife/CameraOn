82424c7c1cda94c802ba7e8d0f682b4b00bd14f1
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-06 19:53:30 +0300

    Update-f4252fd1-6957-47e4-8250-edc1ed59314b

--- src/main/java/com/bss/inc/cameraon/utils/$UI$01.java
+++ src/main/java/com/bss/inc/cameraon/utils/$UI$01.java
@@ -261,7 +261,9 @@ public class $UI$01 {
         }
     }
 
-    public static void u_09$1$4
+    public static void u_09$1$4(TextField[] tfs, CheckBox[] chkbs, RadioButton[] rbs) {
+        
+    }
     public static void u_09$1$1(TextField[] tfs, CheckBox[] chkbs, RadioButton[] rbs) {
         /*
         TextField Array Layout
