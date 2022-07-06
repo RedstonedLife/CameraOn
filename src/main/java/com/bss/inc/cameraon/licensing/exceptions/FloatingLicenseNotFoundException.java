aef0a1ad200df71db12fe80f0983716e2587b72a
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-06 23:10:38 +0300

    Update-2dff33a1-eb99-4893-a980-9505d2544ae0

---
+++ src/main/java/com/bss/inc/cameraon/licensing/exceptions/FloatingLicenseNotFoundException.java
@@ -0,0 +1,5 @@
+package com.bss.inc.cameraon.licensing.exceptions;
+public class FloatingLicenseNotFoundException extends Exception {
+    public FloatingLicenseNotFoundException() {}
+    public FloatingLicenseNotFoundException(String msg) {super(msg);}
+}
