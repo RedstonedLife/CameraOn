3804e0ba2c7deb483d823cc617b6575bb72d1e1b
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-18 16:24:10 +0300

    Update-981c18da-76a0-4683-bb4b-e27418349147

---
+++ pbutils.py
@@ -0,0 +1,9 @@
+import os
+from MD5 import Checksum
+
+path = "src/main/"
+
+for r, d, f in os.walk(path):
+    for file in f:
+        sum = Checksum(os.path.join(r, file))
+        print(sum.createChecksum())
