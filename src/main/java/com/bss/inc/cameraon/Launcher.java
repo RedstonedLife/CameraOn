22c26107105f17d4479cec83cdf16390ac00528f
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-18 22:44:37 +0300

    Update-f7307680-338d-4313-bc3d-1dc79cd3e261

--- src/main/java/com/bss/inc/cameraon/Launcher.java
+++ src/main/java/com/bss/inc/cameraon/Launcher.java
@@ -98,6 +98,6 @@ public class Launcher {
 
     public static void main(String[] args) throws UnknownOS, FileNotFoundException {
         setSettings();
-        MainClass.launch(args);
+        MainClass.main(args);
     }
 }
