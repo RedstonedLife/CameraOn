a120061deba03a4c5ce5bdcb1aec704d530f1149
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-18 16:39:54 +0300

    Update

--- MD5.py
+++ MD5.py
@@ -11,9 +11,37 @@ class Checksum:
                 hash.update(chunk)
         return hash.hexdigest()
 
+    def createSha1(self) -> str:
+        with open(self.file_path, "rb") as f:
+            hash = hashlib.sha1()
+            while chunk := f.read(8192):
+                hash.update(chunk)
+        return hash.hexdigest()
+
+    def createSha256(self) -> str:
+        with open(self.file_path, "rb") as f:
+            hash = hashlib.sha256()
+            while chunk := f.read(8192):
+                hash.update(chunk)
+        return hash.hexdigest()
+
     @staticmethod
     def createStaticChecksum(hashes: tuple) -> str:
         hash = hashlib.md5()
+        for h in hashes:
+            hash.update(bytes(h, 'utf8'))
+        return hash.hexdigest()
+
+    @staticmethod
+    def createStaticSha1(hashes: tuple) -> str:
+        hash = hashlib.sha1()
+        for h in hashes:
+            hash.update(bytes(h, 'utf8'))
+        return hash.hexdigest()
+
+    @staticmethod
+    def createStaticSha256(hashes: tuple) -> str:
+        hash = hashlib.sha256()
         for h in hashes:
             hash.update(bytes(h, 'utf8'))
         return hash.hexdigest()
--- pbutils.py
+++ pbutils.py
@@ -8,9 +8,23 @@ def createTotalChecksum():
     for r, d, f in os.walk(path):
         for file in f:
             sum = Checksum(os.path.join(r, file))
-            sums.append(sum.createChecksum()[1])
+            sums.append(sum.createChecksum())
     return Checksum.createStaticChecksum(sums)
+def createTotalSha():
+    sums = []
+    for r, d, f in os.walk(path):
+        for file in f:
+            sum = Checksum(os.path.join(r, file))
+            sums.append(sum.createSha1())
+    return Checksum.createStaticSha1(sums)
+def createTotalSha256():
+    sums = []
+    for r, d, f in os.walk(path):
+        for file in f:
+            sum = Checksum(os.path.join(r, file))
+            sums.append(sum.createSha256())
+    return Checksum.createStaticSha256(sums)
 with open("checksum.txt", "w+") as f:
-    f.write(createTotalChecksum())
+    f.write(createTotalChecksum() + "\n" + createTotalSha() + "\n" + createTotalSha256())
     f.flush()
     f.close()
