35d5204a108d48cedc89357c81ae40a9dd1de949
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-18 20:38:29 +0300

    Update-5a8317fd-a5f4-4905-9bb0-33d5884a6a43

--- src/main/java/com/bss/inc/cameraon/MainClass.java
+++ src/main/java/com/bss/inc/cameraon/MainClass.java
@@ -68,7 +68,6 @@ public class MainClass extends Application {
     }
 
     public static void main(String[] args) {
-        
     }
     public static DialogManager getWindowManager() {
         return WindowManager;
