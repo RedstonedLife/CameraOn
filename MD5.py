d1c0b4422d2aa719dee48836186a4183a23b640b
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-18 16:16:12 +0300

    Update MD5.py

--- MD5.py
+++ MD5.py
@@ -1,1 +1,12 @@
-import md5
+import hashlib
+
+class Checksum:
+    def __init__(self, file_path: str) -> None:
+        self.file_path = file_path
+
+    def createChecksum(self) -> str:
+        with open(self.file_path, "rb") as f:
+            hash = hashlib.md5()
+            while chunk := f.read(8192):
+                hash.update(chunk)
+        return (hash.digest(), hash.hexdigest())
