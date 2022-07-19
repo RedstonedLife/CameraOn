a06950dfa070f1c9c90e005a9b751128a04376c4
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-19 16:46:57 +0300

    Update-4d99efdd-2555-42bf-8644-3580f494f72e

--- src/main/java/com/bss/inc/cameraon/licensing/exceptions/EncodingDataNotCompleteException.java
+++ src/main/java/com/bss/inc/cameraon/licensing/exceptions/EncodingDataNotCompleteException.java
@@ -1,5 +1,15 @@
 package com.bss.inc.cameraon.licensing.exceptions;
 
 public class EncodingDataNotCompleteException {
+    /* PRIVATE CONSTANTS */
+    private static final long serialVersionUID = 1L;
 
+    /* CONSTRUCTORS */
+    /**
+     * This will create a new instance of a {@link EncodingDataNotCompleteException}.
+     */
+    public EncodingDataNotCompleteException()
+    {
+        super("Encoding-data must be complete for generating product-keys.");
+    }
 }
