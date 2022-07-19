60bc346fc56ada1aae303094d24b7d63963661c3
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-19 17:53:42 +0300

    Update-41c62c0c-92d7-4500-8e57-44399c45648d

--- src/main/java/com/bss/inc/cameraon/settings/Settings.java
+++ src/main/java/com/bss/inc/cameraon/settings/Settings.java
@@ -98,7 +98,7 @@ public class Settings implements ISettings {
         for(String k : settings.keySet()) {
             if(values.containsKey(k)) {values.replace(k, settings.get(k));}
             else {
-                logger.info("Placing " + k + "");
+                logger.info("Placing ");
                 values.putIfAbsent(k, settings.get(k));
             }
         }
