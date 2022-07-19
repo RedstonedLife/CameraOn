00dfddf08d804ce4f3ef1b5a79fec2b21ab6e94a
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-20 00:03:15 +0300

    Update-2fbac4b3-128e-427f-9dee-342a431c0c6a

--- src/main/java/com/bss/inc/cameraon/licensing/exceptions/SeedAlreadyTakenException.java
+++ src/main/java/com/bss/inc/cameraon/licensing/exceptions/SeedAlreadyTakenException.java
@@ -1,4 +1,19 @@
 package com.bss.inc.cameraon.licensing.exceptions;
 
-public class SeedAlreadyTakenException {
+public class SeedAlreadyTakenException extends ProductKeyGenerationException
+{
+
+
+    /* PRIVATE CONSTANTS */
+    private static final long serialVersionUID = 1L;
+
+    /* CONSTRUCTORS */
+    /**
+     * This will create a new instance of a {@link SeedAlreadyTakenException}.
+     */
+    public SeedAlreadyTakenException()
+    {
+        super("Seed is already taken.");
+    }
+
 }
