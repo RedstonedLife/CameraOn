5f7ec9a99faa9b1b8a902e29a62bc2093e4e6fb3
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-20 00:03:29 +0300

    Update-d121b445-44b7-4d0f-b1a1-f341ba1e483d

--- src/main/java/com/bss/inc/cameraon/licensing/exceptions/SeedIsBlacklistedException.java
+++ src/main/java/com/bss/inc/cameraon/licensing/exceptions/SeedIsBlacklistedException.java
@@ -1,4 +1,19 @@
 package com.bss.inc.cameraon.licensing.exceptions;
 
-public class SeedIsBlacklistedException {
+public class SeedIsBlacklistedException extends ProductKeyGenerationException
+{
+
+
+    /* PRIVATE CONSTANTS */
+    private static final long serialVersionUID = 1L;
+
+    /* CONSTRUCTORS */
+    /**
+     * This will create a new instance of a {@link SeedIsBlacklistedException}.
+     */
+    public SeedIsBlacklistedException()
+    {
+        super("Seed provided is black-listed.");
+    }
+
 }
