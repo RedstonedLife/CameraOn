f973eadeaaa0263a19ef28e4a69577f9c116abfa
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-20 00:02:44 +0300

    Update-95d8541f-0fc4-4276-9e41-5e8083c1d1c1

--- src/main/java/com/bss/inc/cameraon/licensing/exceptions/ProductKeyEncoderNotDefinedException.java
+++ src/main/java/com/bss/inc/cameraon/licensing/exceptions/ProductKeyEncoderNotDefinedException.java
@@ -1,4 +1,15 @@
 package com.bss.inc.cameraon.licensing.exceptions;
 
-public class ProductKeyEncoderNotDefinedException {
+public class ProductKeyEncoderNotDefinedException extends ProductKeyGenerationException {
+    /* PRIVATE CONSTANTS */
+    private static final long serialVersionUID = 1L;
+
+    /* CONSTRUCTORS */
+    /**
+     * This will create a new instance of a {@link ProductKeyEncoderNotDefinedException}.
+     */
+    public ProductKeyEncoderNotDefinedException()
+    {
+        super("Product-key encoder is not defined.");
+    }
 }
