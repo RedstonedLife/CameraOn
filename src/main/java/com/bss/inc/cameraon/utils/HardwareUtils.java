1537696e12ac7b3457d6b8bd6e20abe71a79ad3f
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-20 21:01:17 +0300

    Update-75ba3896-efa3-4573-9af8-e7ee6b33c874

--- src/main/java/com/bss/inc/cameraon/utils/HardwareUtils.java
+++ src/main/java/com/bss/inc/cameraon/utils/HardwareUtils.java
@@ -83,7 +83,7 @@ public class HardwareUtils {
         Logger.getLogger("BNC").info(tl("hardwareinfo.firmware.osname", os.getOSF()));
         Logger.getLogger("BNC").info(tl("hardwareinfo.firmware.osfamily", os.getFamily()));
         Logger.getLogger("BNC").info(tl("hardwareinfo.firmware.osmanufacturer", os.getManufacturer()));
-        Logger.getLogger("BNC").info(tl("hardwareinfo.firmware."));
+        Logger.getLogger("BNC").info(tl("hardwareinfo.firmware.osbita"));
         Logger.getLogger("BNC").info(tl("hardwareinfo.line1"));
         Logger.getLogger("BNC").info(tl("hardwareinfo.line1"));
         Logger.getLogger("BNC").info(tl("hardwareinfo.line1"));
