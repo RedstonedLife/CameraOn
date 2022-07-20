22a13cef1b65eab32a80ae301bc70e30181c7466
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-21 01:12:37 +0300

    Update-24316a8e-3184-43ca-b031-582d80d5a15b

--- src/main/java/com/bss/inc/cameraon/utils/$UI$01.java
+++ src/main/java/com/bss/inc/cameraon/utils/$UI$01.java
@@ -26,7 +26,7 @@ public class $UI$01 {
 
     static {
         digitPattern = Pattern.compile("\\d+");
-        licensePattern = Pattern.compile("[A-Z\\d]");
+        licensePattern = Pattern.compile("[A-Z\\d]{4}\\-|[A-Z\\d]{4}");
     }
 
     public static void u_00$1$1(SplitPane csplitpane) {csplitpane.getDividers().get(0).positionProperty().addListener((observableValue, number, t1) -> csplitpane.getDividers().get(0).setPosition(FrontendPaths.DIVID_POS));}
