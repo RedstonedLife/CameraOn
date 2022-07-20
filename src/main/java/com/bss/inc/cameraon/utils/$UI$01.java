d077ebf01ad1fc7de95b0bf37cff43e5a410660d
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-21 01:25:14 +0300

    Update-578e7cf3-edd5-4483-901e-d0dcbaa3dd27

--- src/main/java/com/bss/inc/cameraon/utils/$UI$01.java
+++ src/main/java/com/bss/inc/cameraon/utils/$UI$01.java
@@ -319,11 +319,10 @@ public class $UI$01 {
             int finalI = i;
             textFields[i].textProperty().addListener((observable, oldValue, newValue) -> {
                 if(oldValue == null || oldValue == "") {return;}
-                if((newValue.length() > 4)) {boolean b = finalI < textFields.length;
+                if((newValue.length() > 4)) {
+                    boolean b = finalI < textFields.length;
                     if(b){return;}
-                    else {textFields[finalI].setFocusTraversable(false);textFields[finalI+1].setFocusTraversable(true);}
-                if((newValue.length() > 4)) {textFields[finalI].setText(oldValue);
-                }
+                    else {textFields[finalI].setFocusTraversable(false);textFields[finalI+1].setFocusTraversable(true);textFields[finalI].setText(oldValue);}}
             });
         }
     }
