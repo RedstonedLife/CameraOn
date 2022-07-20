6175eb0f7a4f17de5901b6414868e3fb5686b429
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-20 20:24:57 +0300

    Update-5a5c27d2-7e83-4d59-a388-97274b0eac72

--- src/main/java/com/bss/inc/cameraon/utils/HardwareUtils.java
+++ src/main/java/com/bss/inc/cameraon/utils/HardwareUtils.java
@@ -1,9 +1,11 @@
 package com.bss.inc.cameraon.utils;
 
 import oshi.SystemInfo;
+import oshi.hardware.HardwareAbstractionLayer;
 
 public class HardwareUtils {
     private final SystemInfo systemInfo;
     public HardwareUtils() {this.systemInfo = new SystemInfo();}
-    
+    public HardwareAbstractionLayer getHardware() {return this.systemInfo.getHardware();
+    }
 }
