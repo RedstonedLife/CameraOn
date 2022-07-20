70912e89ef5693aa2681e099c3642e6b82f57518
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-21 01:43:44 +0300

    Update-e81b2394-1a74-4e83-b232-bcf400f45858

--- src/main/java/com/bss/inc/cameraon/utils/$UI$01.java
+++ src/main/java/com/bss/inc/cameraon/utils/$UI$01.java
@@ -316,7 +316,9 @@ public class $UI$01 {
 
     public static void u_11$1$1(TextField[] textFields) {
         textFields[0].textProperty().addListener((observable, oldValue, newValue) -> {
-            if(newValue.split("-").length==5)
+            if(newValue.split("-").length==5) {
+                
+            }
             if(oldValue == null || oldValue == "") {return;}
             if((newValue.length() == 8)) {
                 textFields[1].requestFocus();textFields[0].setText(oldValue);}
