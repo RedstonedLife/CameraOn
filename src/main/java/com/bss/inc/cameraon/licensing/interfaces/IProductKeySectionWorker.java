fba975ccdea71752d9c4553257057e81470182e0
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-20 17:14:04 +0300

    Update-f565525b-2433-4d62-8ff8-4026f25cf02a

--- src/main/java/com/bss/inc/cameraon/licensing/interfaces/IProductKeySectionWorker.java
+++ src/main/java/com/bss/inc/cameraon/licensing/interfaces/IProductKeySectionWorker.java
@@ -1,5 +1,5 @@
 package com.bss.inc.cameraon.licensing.interfaces;
 
-public interface IProductKeySectionWorker<ED extends ProductKeyEncodingData> {
+public interface IProductKeySectionWorker<ED extends IProductKeyEncodingData> {
     byte buildProductKeySection(final long seed, ED productKeyEncodingDate);
 }
