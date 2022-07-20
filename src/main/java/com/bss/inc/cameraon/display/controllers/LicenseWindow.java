3d634dad8e1c6297215dc23a52b86c103fa9183b
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-21 01:40:16 +0300

    Update-611cf5a8-fd82-4ae5-a89e-49b0f8249a43

--- src/main/java/com/bss/inc/cameraon/display/controllers/LicenseWindow.java
+++ src/main/java/com/bss/inc/cameraon/display/controllers/LicenseWindow.java
@@ -15,7 +15,7 @@ import java.util.ResourceBundle;
 
 public class LicenseWindow implements Initializable {
     @FXML private Text TitleTxt, SubTitleTxt;
-    @FXML private TextField ACtf1,ACtf2,ACtf3,ACtf4;
+    @FXML private TextField ACtf1,ACtf2,ACtf3,ACtf4,ACtf5;
     @FXML private Button HelpBtn, ActivateBtn, btnMinimize, btnClose;
 
     @Override
@@ -23,7 +23,7 @@ public class LicenseWindow implements Initializable {
         HelpBtn.setCancelButton(true);
         ActivateBtn.setOnAction(e -> MainClass.getWindowManager().changeDialogScene("licenseWindow", MainClass.class.getResource(FrontendPaths.LICENSE_KEY_FXML)));
         btnClose.setOnAction(e -> MainClass.getWindowManager().changeDialogScene("welcomeWindow", MainClass.class.getResource(FrontendPaths.WELCOME_NA_FXML)));
-        $UI$01.u_11$1$1(new TextField[]{ACtf1,ACtf2,ACtf3,ACtf4});
+        $UI$01.u_11$1$1(new TextField[]{ACtf1,ACtf2,ACtf3,ACtf4,ACtf5});
         $UI$01.u_10$1$2(btnMinimize, "welcomeWindow");
     }
 }
