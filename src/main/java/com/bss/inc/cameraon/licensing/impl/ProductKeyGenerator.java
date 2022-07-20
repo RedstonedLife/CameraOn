347754e3cf939b583d4be789c8565c734bfd693b
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-20 17:49:34 +0300

    Update-cf484c76-6d21-48a5-bdfe-0d9208afc39d

--- src/main/java/com/bss/inc/cameraon/licensing/impl/ProductKeyGenerator.java
+++ src/main/java/com/bss/inc/cameraon/licensing/impl/ProductKeyGenerator.java
@@ -1,8 +1,37 @@
 package com.bss.inc.cameraon.licensing.impl;
 
 import com.bss.inc.cameraon.licensing.impl.beans.ProductKeyEncodingData;
+import com.bss.inc.cameraon.licensing.interfaces.IProductKeySectionWorker;
 
 public class ProductKeyGenerator extends com.bss.inc.cameraon.licensing.ProductKeyGenerator<ProductKeyEncodingData> {
     public ProductKeyGenerator(final ProductKeyEncodingData...productKeyEncodingData) {super(8, productKeyEncodingData);}
-    
+    @Override
+    protected IProductKeySectionWorker<ProductKeyEncodingData> buildProductKeySectionWorker()
+    {
+        return new BasicProductKeySectionWorker();
+    }
+
+    @Override
+    protected IChecksumWorker buildChecksumWorker()
+    {
+        return new BasicChecksumWorker();
+    }
+
+    @Override
+    protected IBlacklistWorker buildBlacklistWorker()
+    {
+        return null;
+    }
+
+    @Override
+    protected IProductKeyStylingWorker buildProductKeyStylingWorker()
+    {
+        return new BasicProductKeyStylingWorker();
+    }
+
+    @Override
+    protected ISeedAvailabilityWorker buildSeedAvailabilityWorker()
+    {
+        return null;
+    }
 }
