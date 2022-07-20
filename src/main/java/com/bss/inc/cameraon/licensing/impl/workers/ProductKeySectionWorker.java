caff0a4a2e5fe104d7061635148e2d91537ac507
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-20 17:19:01 +0300

    Update-e6f77d3b-c61d-478d-8394-428172ee8458

--- src/main/java/com/bss/inc/cameraon/licensing/impl/workers/ProductKeySectionWorker.java
+++ src/main/java/com/bss/inc/cameraon/licensing/impl/workers/ProductKeySectionWorker.java
@@ -3,19 +3,5 @@ package com.bss.inc.cameraon.licensing.impl.workers;
 import com.bss.inc.cameraon.licensing.interfaces.IProductKeySectionWorker;
 
 public class ProductKeySectionWorker implements IProductKeySectionWorker<ProductKeyEncodingData> {
-    @Override
-    public byte buildProductKeySection(final long seed, final ProductKeyEncodingData encodingData)
-    {
-        byte a = (byte) (((int) encodingData.getA()) % 25);
-        byte b = (byte) (((int) encodingData.getB()) % 3);
-
-        if (((int) a) % 2 == 0)
-        {
-            return (byte) (((seed >> a) & 0x000000FF) ^ ((seed >> b) | encodingData.getC()));
-        }
-        else
-        {
-            return (byte) (((seed >> a) & 0x000000FF) ^ ((seed >> b) & encodingData.getC()));
-        }
-    }
+    @Override public byte buildProductKeySection(final long seed, final ProductKeyEncodingData encodingData) {byte a = (byte) (((int) encodingData.getA()) % 25);byte b = (byte) (((int) encodingData.getB()) % 3);if (((int) a) % 2 == 0) {return (byte) (((seed >> a) & 0x000000FF) ^ ((seed >> b) | encodingData.getC()));} else {return (byte) (((seed >> a) & 0x000000FF) ^ ((seed >> b) & encodingData.getC()));}}
 }
