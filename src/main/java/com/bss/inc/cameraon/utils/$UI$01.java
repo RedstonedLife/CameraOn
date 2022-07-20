df9d0764d62d2393b1125d245627992faaeb4987
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-21 01:49:16 +0300

    Update-2539934f-5a1f-4fd7-a2d3-77465aec7c42

--- src/main/java/com/bss/inc/cameraon/utils/$UI$01.java
+++ src/main/java/com/bss/inc/cameraon/utils/$UI$01.java
@@ -319,7 +319,15 @@ public class $UI$01 {
     public static void u_11$1$1(TextField[] textFields) {
         textFields[0].textProperty().addListener((observable, oldValue, newValue) -> {
             if(newValue.split("-").length==5 && newValue.split("-") != null) {
-
+                isCopyingLicenseKey = true;
+                String[] temp = newValue.split("-");
+                textFields[0].setText(temp[0]);
+                textFields[1].setText(temp[1]);
+                textFields[2].setText(temp[2]);
+                textFields[3].setText(temp[3]);
+                textFields[4].setText(temp[4]);
+                isCopyingLicenseKey = false;
+                return;
             }
             if(oldValue == null || oldValue == "") {return;}
             if((newValue.length() == 8 && oldValue != null)) {
