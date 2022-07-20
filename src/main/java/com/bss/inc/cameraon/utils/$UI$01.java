89a68a96773f8ce4b99201b11c0e3a9ee752a130
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-21 01:30:13 +0300

    Update-80521c07-d66c-4f13-9868-a53095d8711b

--- src/main/java/com/bss/inc/cameraon/utils/$UI$01.java
+++ src/main/java/com/bss/inc/cameraon/utils/$UI$01.java
@@ -331,7 +331,9 @@ public class $UI$01 {
                 textFields[2].setFocusTraversable(false);textFields[3].requestFocus();textFields[2].setText(oldValue);}
         });
         textFields[3].textProperty().addListener((observable, oldValue, newValue) -> {
-            if(oldValue == null || oldValue == "") {return;}
+            if(oldValue == null || oldValue == "") {
+                
+            }
             if((newValue.length() >= 5)) {
                 textFields[3].setText(oldValue);}
         });
