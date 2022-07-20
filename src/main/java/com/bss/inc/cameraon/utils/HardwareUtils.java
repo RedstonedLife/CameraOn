8570ca1584ab8d4524e002e0531f7e7354ff9bd4
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-20 20:53:20 +0300

    Update-1d83ba41-b441-4e7d-9fc1-2cb8027d5eaf

--- src/main/java/com/bss/inc/cameraon/utils/HardwareUtils.java
+++ src/main/java/com/bss/inc/cameraon/utils/HardwareUtils.java
@@ -51,7 +51,7 @@ public class HardwareUtils {
         Logger.getLogger("BNC").info(tl("hardwareinfo.cpu.vendor", cpu.getVendor()));
         Logger.getLogger("BNC").info(tl("hardwareinfo.cpu.name", cpu.getName()));
         Logger.getLogger("BNC").info(tl("hardwareinfo.cpu.id", cpu.getId()));
-        Logger.getLogger("BNC").info(tl("hardwareinfo.cpu"));
+        Logger.getLogger("BNC").info(tl("hardwareinfo.cpu."));
     }
 }
 
