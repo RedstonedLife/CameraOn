93e0bccd12423c18940d5b820f355875b1b7f36a
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-20 17:27:42 +0300

    Update-aa233c46-eada-454d-8c4e-b6eab30365f3

--- src/main/java/com/bss/inc/cameraon/licensing/ProductKeyGenerator.java
+++ src/main/java/com/bss/inc/cameraon/licensing/ProductKeyGenerator.java
@@ -3,5 +3,5 @@ package com.bss.inc.cameraon.licensing;
 import com.bss.inc.cameraon.licensing.impl.beans.ProductKeyEncodingData;
 
 public abstract class ProductKeyGenerator<ED extends ProductKeyEncodingData> {
-    
+    private final int seedCharLength;
 }
