3521b40dee703cdcd531d046cc263ae6e45054b7
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-20 17:42:16 +0300

    Update-d4db9f9c-b7db-4c87-aaf2-5d486753cbc8

--- src/main/java/com/bss/inc/cameraon/licensing/ProductKeyGenerator.java
+++ src/main/java/com/bss/inc/cameraon/licensing/ProductKeyGenerator.java
@@ -57,19 +57,7 @@ public abstract class ProductKeyGenerator<ED extends ProductKeyEncodingData> {
         try {
             // Try to parse the HEXADECIMAL string representing the seed and use that for the seed.
             seed = Long.parseLong(seedHex, 16);
-            if ((this.productKeyEncodingData != null) && (this.productKeyEncodingData.length > 0)) {if (this.getProductKeySectionWorker() != null) {
-                final StringBuilder keySb = new StringBuilder();
-
-                    seedHex = ProductKeyUtils.buildHexStr(this.getSeedCharLength(), seed);
-
-                    if ((this.getBlacklistWorker() != null) && (this.getBlacklistWorker().isSeedBlackListed(seedHex)))
-                    {
-                        throw new SeedIsBlacklistedException();
-                    }
-
-                    if ((this.getSeedAvailabilityWorker() != null) && ( !this.getSeedAvailabilityWorker().isSeedAvailable(seedHex)))
-                    {
-                        throw new SeedAlreadyTakenException();
+            if ((this.productKeyEncodingData != null) && (this.productKeyEncodingData.length > 0)) {if (this.getProductKeySectionWorker() != null) {final StringBuilder keySb = new StringBuilder();seedHex = ProductKeyUtils.buildHexStr(this.getSeedCharLength(), seed);if ((this.getBlacklistWorker() != null) && (this.getBlacklistWorker().isSeedBlackListed(seedHex))) {throw new SeedIsBlacklistedException();}if ((this.getSeedAvailabilityWorker() != null) && ( !this.getSeedAvailabilityWorker().isSeedAvailable(seedHex))) throw new SeedAlreadyTakenException();
                     }
 
                     // The key string begins with a HEXADECIMAL string of the seed.
