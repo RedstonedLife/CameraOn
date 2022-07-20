a048ee508fbe3978c10ae8dd60ad0428d060f786
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-20 17:28:19 +0300

    Create ProductKeyGenerator.java

---
+++ src/main/java/com/bss/inc/cameraon/licensing/impl/ProductKeyGenerator.java
@@ -0,0 +1,7 @@
+package com.bss.inc.cameraon.licensing.impl;
+
+import com.bss.inc.cameraon.licensing.impl.beans.ProductKeyEncodingData;
+
+public class ProductKeyGenerator extends com.bss.inc.cameraon.licensing.ProductKeyGenerator<ProductKeyEncodingData> {
+    public ProductKeyGenerator(final ProductKeyEncodingData...productKeyEncodingData) {super(8, productKeyEncodingData);}
+}
