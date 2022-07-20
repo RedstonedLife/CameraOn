f8eb2f39b9105ed0ba4b7505b993b98851bd5e83
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-20 17:09:17 +0300

    Update-a9a99c1b-8acd-4c90-b13f-ac025ee4626c

--- src/main/java/com/bss/inc/cameraon/licensing/utils/ProductKeyUtils.java
+++ src/main/java/com/bss/inc/cameraon/licensing/utils/ProductKeyUtils.java
@@ -2,6 +2,8 @@ package com.bss.inc.cameraon.licensing.utils;
 
 public class ProductKeyUtils {
     public static String buildHexStr(final int expectedLength, final long content) {
-        String hexStr = String.format("%0")
+        String hexStr = String.format("%0" + expectedLength + "X", content) {
+            
+        }
     }
 }
