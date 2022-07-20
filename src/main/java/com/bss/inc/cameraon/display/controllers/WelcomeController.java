c5f118dcbf57d5da7adfe49c0a01f81862850339
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-21 00:53:30 +0300

    Update-e9ee0a05-45be-4351-abe2-04a154cbf8b5

--- src/main/java/com/bss/inc/cameraon/display/controllers/WelcomeController.java
+++ src/main/java/com/bss/inc/cameraon/display/controllers/WelcomeController.java
@@ -2,10 +2,12 @@ package com.bss.inc.cameraon.display.controllers;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.text.Text;
 
 public class WelcomeController implements Initializable {
     @FXML private Text licensetxt,versiontxt,showTxt;
     @FXML private CheckBox showChkB;
+    @FXML private Button
 }
