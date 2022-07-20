7b319835f6bd462a9e642e0cda36c86e6b7bff58
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-21 01:29:38 +0300

    Update-9fa16e58-ff3b-46eb-ba25-4f37fa6d7b99

--- src/main/java/com/bss/inc/cameraon/utils/$UI$01.java
+++ src/main/java/com/bss/inc/cameraon/utils/$UI$01.java
@@ -317,22 +317,22 @@ public class $UI$01 {
     public static void u_11$1$1(TextField[] textFields) {
         textFields[0].textProperty().addListener((observable, oldValue, newValue) -> {
             if(oldValue == null || oldValue == "") {return;}
-            if((newValue.length() >= 4)) {
+            if((newValue.length() >= 5)) {
                 textFields[0].setFocusTraversable(false);textFields[1].requestFocus();textFields[0].setText(oldValue);}
         });
         textFields[1].textProperty().addListener((observable, oldValue, newValue) -> {
             if(oldValue == null || oldValue == "") {return;}
-            if((newValue.length() >= 4)) {
+            if((newValue.length() >= 5)) {
                 textFields[1].setFocusTraversable(false);textFields[2].requestFocus();textFields[1].setText(oldValue);}
         });
         textFields[2].textProperty().addListener((observable, oldValue, newValue) -> {
             if(oldValue == null || oldValue == "") {return;}
-            if((newValue.length() >= 4)) {
+            if((newValue.length() >= 5)) {
                 textFields[2].setFocusTraversable(false);textFields[3].requestFocus();textFields[2].setText(oldValue);}
         });
         textFields[3].textProperty().addListener((observable, oldValue, newValue) -> {
             if(oldValue == null || oldValue == "") {return;}
-            if((newValue.length() >= 4)) {
+            if((newValue.length() >= 5)) {
                 textFields[3].setText(oldValue);}
         });
     }
