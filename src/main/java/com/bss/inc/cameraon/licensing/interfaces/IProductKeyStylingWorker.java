ac32c26e7a428580358dd0b896eb32289df4c57d
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-20 17:13:36 +0300

    Create IProductKeyStylingWorker.java

---
+++ src/main/java/com/bss/inc/cameraon/licensing/interfaces/IProductKeyStylingWorker.java
@@ -0,0 +1,6 @@
+package com.bss.inc.cameraon.licensing.interfaces;
+
+public interface IProductKeyStylingWorker {
+    String addStyling(String productKey);
+    String removeStyling(String productKey);
+}
