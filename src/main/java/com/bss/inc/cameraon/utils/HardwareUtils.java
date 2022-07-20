f1438eee895a1f82053d9429bd762e1494cb90b7
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-20 20:55:26 +0300

    Update-f5f98262-13cb-4a2c-8ce7-6ef71339f0e9

--- src/main/java/com/bss/inc/cameraon/utils/HardwareUtils.java
+++ src/main/java/com/bss/inc/cameraon/utils/HardwareUtils.java
@@ -58,7 +58,7 @@ public class HardwareUtils {
         Logger.getLogger("BNC").info(tl("hardwareinfo.baseboard.manufacturer", baseboardInfo.getManufacturer()));
         Logger.getLogger("BNC").info(tl("hardwareinfo.baseboard.model"));
         Logger.getLogger("BNC").info(tl("hardwareinfo.baseboard.serial"));
-        Logger.getLogger("BNC").info(tl("hardwareinfo.line1"));
+        Logger.getLogger("BNC").info(tl("hardwareinfo.baseboard."));
     }
 }
 
