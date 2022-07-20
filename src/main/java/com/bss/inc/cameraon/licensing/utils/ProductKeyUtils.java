96e5d4ea2d772d32fc39e77ec77076d080696d50
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-20 17:09:51 +0300

    Update-e417050f-a4d7-4680-b3d4-0a37955c1f21

--- src/main/java/com/bss/inc/cameraon/licensing/utils/ProductKeyUtils.java
+++ src/main/java/com/bss/inc/cameraon/licensing/utils/ProductKeyUtils.java
@@ -3,6 +3,7 @@ package com.bss.inc.cameraon.licensing.utils;
 public class ProductKeyUtils {
     public static String buildHexStr(final int expectedLength, final long content) {
         String hexStr = String.format("%0" + expectedLength + "X", content);
-        if (hexStr.length() > expectedLength)
+        if (hexStr.length() > expectedLength) hexStr.substring(hexStr.length() - expectedLength);
+        while(hexStr.length() < expectedLength)
     }
 }
