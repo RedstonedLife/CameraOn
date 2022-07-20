809989722f3850c4bd06b5e41590a32ccc117141
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-20 17:10:53 +0300

    Update-d83d611c-e1b8-4769-86b9-854db5ba215a

--- src/main/java/com/bss/inc/cameraon/licensing/interfaces/IBlacklistWorker.java
+++ src/main/java/com/bss/inc/cameraon/licensing/interfaces/IBlacklistWorker.java
@@ -2,5 +2,5 @@ package com.bss.inc.cameraon.licensing.interfaces;
 
 public interface IBlacklistWorker {
     boolean isKeyBlackListed(String productKey);
-    
+    boolean isSeedBlackListed(String seedHex);
 }
