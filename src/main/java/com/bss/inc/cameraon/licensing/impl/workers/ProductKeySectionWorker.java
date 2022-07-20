a2c4c35c04b225cc29a4236bac18f05ad5a7a141
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-20 17:18:29 +0300

    Update-61dd441a-10e6-432d-bcdd-affc9f9e3a95

--- src/main/java/com/bss/inc/cameraon/licensing/impl/workers/ProductKeySectionWorker.java
+++ src/main/java/com/bss/inc/cameraon/licensing/impl/workers/ProductKeySectionWorker.java
@@ -3,4 +3,5 @@ package com.bss.inc.cameraon.licensing.impl.workers;
 import com.bss.inc.cameraon.licensing.interfaces.IProductKeySectionWorker;
 
 public class ProductKeySectionWorker implements IProductKeySectionWorker<ProductKeyEncodingData> {
+    
 }
