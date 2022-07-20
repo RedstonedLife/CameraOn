d2adbc2b20fee6e559c37a0a393a165c4c9955cd
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-20 20:58:56 +0300

    Update-11e7c172-bd93-4c27-812b-744e841d9b66

--- src/main/java/com/bss/inc/cameraon/utils/HardwareUtils.java
+++ src/main/java/com/bss/inc/cameraon/utils/HardwareUtils.java
@@ -72,7 +72,7 @@ public class HardwareUtils {
             Logger.getLogger("BNC").info(tl("hardwareinfo.storage.options", sdev.getOptions()));
             Logger.getLogger("BNC").info(tl("hardwareinfo.storage.type", sdev.getType()));
             Logger.getLogger("BNC").info(tl("hardwareinfo.storage.uuid", sdev.getUUID()));
-            Logger.getLogger("BNC").info(tl("hardwareinfo.line1"));
+            Logger.getLogger("BNC").info(tl("hardwareinfo.storage.vou"));
             Logger.getLogger("BNC").info(tl("hardwareinfo.line1"));
         }
     }
