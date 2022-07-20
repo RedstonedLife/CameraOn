5b9e19e0bace7482e72f248e7a9bbde4e97f5ad7
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-21 01:00:44 +0300

    Update-fe966900-72de-40e4-9c62-ae59d8926cd7

--- src/main/java/com/bss/inc/cameraon/display/controllers/LicenseWindow.java
+++ src/main/java/com/bss/inc/cameraon/display/controllers/LicenseWindow.java
@@ -1,4 +1,27 @@
 package com.bss.inc.cameraon.display.controllers;
 
-public class LicenseWindow {
-}
+import com.bss.inc.cameraon.MainClass;
+import com.bss.inc.cameraon.constants.FrontendPaths;
+import com.bss.inc.cameraon.utils.$UI$01;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.text.Text;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LicenseWindow implements Initializable {
+    @FXML
+    private Text licensetxt,versiontxt,showTxt;
+    @FXML private CheckBox showChkB;
+    @FXML private Button HelpBtn,StartBtn,ActivateBtn,btnMinimize,btnClose;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        HelpBtn.setCancelButton(true);
+        ActivateBtn.setOnAction(e -> MainClass.getWindowManager().changeDialogScene("licenseWindow", MainClass.class.getResource(FrontendPaths.LICENSE_KEY_FXML)));
+        $UI$01.u_10$1$1(btnClose);
+        $UI$01.u_10$1$2(btnMinimize,"welcomeWindow");
+    }
