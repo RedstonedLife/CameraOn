d80d66b2515b0c54fb688b50d29300074658f9da
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-21 00:38:36 +0300

    Update-1f9077db-c7e9-432e-9acb-bd6ae38d7257

--- src/main/java/com/bss/inc/cameraon/MainClass.java
+++ src/main/java/com/bss/inc/cameraon/MainClass.java
@@ -53,7 +53,7 @@ public class MainClass extends Application {
                         clazz.getResource(FrontendPaths.POP_FAQ_FXML),clazz.getResource(FrontendPaths.POP_TPL_FXML),
                         clazz.getResource(FrontendPaths.POP_HELP_FXML)
                 }), // PopUpWindow URL[] End
-                new Dialog(500d, 200d)
+                new Dialog(500d, 200d, "license")
         });}
         catch (IOException e) {logger.error("Error occurred while initializing the WindowManager", e);e.printStackTrace();}
         WindowManager.changeDialogScene("mainWindow",clazz.getResource(FrontendPaths.SETTINGS_FXML));
