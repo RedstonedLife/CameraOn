131910381ee59d3cfc55d1843be795e53a0abb60
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-22 00:43:12 +0300

    Update-16508e05-3532-493e-98bf-af9e3462c1d3

--- src/main/java/com/bss/inc/cameraon/display/controllers/Scan.java
+++ src/main/java/com/bss/inc/cameraon/display/controllers/Scan.java
@@ -44,8 +44,8 @@ public class Scan implements Initializable {
             UI02.setEndIpAddress(iprangetoTf.getText());
             switch (MainClass.getStateMachine().getState()) {
                 case SCANNING -> {scanBtn.setDisable(true);break;}
-                case STOPPING -> {scanBtn.setDisable(true);break;}
-                case KILLING -> {scanBtn.setDisable(true);break;}
+                case STOPPING -> {scanBtn.setDisable(false);break;}
+                case KILLING -> {break;}
                 case RESTARTING -> {scanBtn.setDisable(true);break;}
                 case STARTING -> {scanBtn.setDisable(true);break;}
                 case IDLE -> {break;}
