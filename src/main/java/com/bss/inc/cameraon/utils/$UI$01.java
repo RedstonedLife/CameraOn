9d50b39e22b5b5ecfb61ad4edf5c63a4f4f65b63
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-21 14:00:03 +0300

    Update-6d8bf074-cca6-4d76-aa89-4d842987486a

--- src/main/java/com/bss/inc/cameraon/utils/$UI$01.java
+++ src/main/java/com/bss/inc/cameraon/utils/$UI$01.java
@@ -436,7 +436,7 @@ public class $UI$01 {
 
     public static void u_11$1$4(TextField[] textFields, Rectangle[] rectangles) {
         if(licenseHandler.verifyProductKey($UI$01.u_11$1$2(textFields)) != ProductKeyState.KEY_GOOD) {
-
+            
         }
     }
 
