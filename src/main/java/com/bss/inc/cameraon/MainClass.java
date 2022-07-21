5e2b02b05f5bfe2322f3856d58310a98cdccc94c
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-21 15:30:26 +0300

    Update-9e1a6134-a272-4515-bf62-b5d99ee7c40b

--- src/main/java/com/bss/inc/cameraon/MainClass.java
+++ src/main/java/com/bss/inc/cameraon/MainClass.java
@@ -70,8 +70,8 @@ public class MainClass extends Application {
         catch (IOException e) {logger.error("Error occurred while initializing the WindowManager", e);e.printStackTrace();}
         if(regi.get("ST", null) == null && regi.get("isAT", null) == null) {
             WindowManager.changeDialogScene("welcomeWindow", clazz.getResource(FrontendPaths.WELCOME_NA_FXML));
-        } else {
-            
+        } else if(regi.get("ST", null) != null && regi.get("isAT", null) != null) {
+            WindowManager.changeDialogScene("welcomeWindow", clazz.getResource(FrontendPaths.WELCOME_FXML));
         }
         WindowManager.openDialog("welcomeWindow");
         //WindowManager.changeDialogScene("mainWindow", clazz.getResource(FrontendPaths.SETTINGS_FXML));
