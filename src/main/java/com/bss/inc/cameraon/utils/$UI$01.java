259d1986a21448a170508f1220cb488f4647be32
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-21 14:10:46 +0300

    Update-dc5f956f-eaa2-466b-adce-8922f22831ed

--- src/main/java/com/bss/inc/cameraon/utils/$UI$01.java
+++ src/main/java/com/bss/inc/cameraon/utils/$UI$01.java
@@ -426,7 +426,7 @@ public class $UI$01 {
 
     public static void u_11$1$3(Rectangle[] rectangles) {
         for(Rectangle rect : rectangles) {
-            rect.setFill(Color.color(253,19,19));
+            rect.setFill(Color.color(253d,19d,19d));
         }
         try {TimeUnit.SECONDS.sleep(1);} catch (InterruptedException e) {throw new RuntimeException(e);}
         for(Rectangle rect : rectangles) {
