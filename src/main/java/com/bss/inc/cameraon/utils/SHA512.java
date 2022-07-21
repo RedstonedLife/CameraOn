cbe30f9c56cefd1d6bbdec00c30c40a94158707b
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-21 14:41:04 +0300

    Update-9066a4e0-8428-43ef-b45e-9f15a8fab981

--- src/main/java/com/bss/inc/cameraon/utils/SHA512.java
+++ src/main/java/com/bss/inc/cameraon/utils/SHA512.java
@@ -1,7 +1,13 @@
 package com.bss.inc.cameraon.utils;
 
+import java.security.MessageDigest;
+
 public class SHA512 {
     public static String getSecureSHA(final String string, final byte[] salt) {
-        
+        String temp = null;
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-512");
+        }
+        return null;
     }
 }
