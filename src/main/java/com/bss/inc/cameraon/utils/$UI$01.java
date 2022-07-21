69cd7a96505666d8137d45dd39a2b2235ccb8927
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-21 15:03:42 +0300

    Update-d08e1156-f933-4e57-9a67-ded6ae338e65

--- src/main/java/com/bss/inc/cameraon/utils/$UI$01.java
+++ src/main/java/com/bss/inc/cameraon/utils/$UI$01.java
@@ -441,11 +441,11 @@ public class $UI$01 {
             u_11$1$3(rectangles);
         } else if(licenseHandler.verifyProductKey($UI$01.u_11$1$2(textFields)) == ProductKeyState.KEY_GOOD) {
             try {WinRegistry.writeStringValue(WinRegistry.HKEY_CURRENT_USER, "SOFTWARE\\BSS\\CMON", "isAT", SHA512.getSecureSHA("true&&"+Launcher.getHu().getFileStores().get(0).getUUID(),
-                    WinRegistry.readString(WinRegistry.HKEY_CURRENT_USER, "SOFTWARE\\BSS\\CMON", "ST").getBytes()), );}
+                    WinRegistry.readString(WinRegistry.HKEY_CURRENT_USER, "SOFTWARE\\BSS\\CMON", "ST").getBytes()), WinRegistry.KEY_WOW64_64KEY);}
             catch (IllegalAccessException e) {throw new RuntimeException(e);}
             catch (InvocationTargetException e) {throw new RuntimeException(e);}
             try {
-                System.out.println(WinRegistry.readString(WinRegistry.HKEY_CURRENT_USER, "SOFTWARE\\BSS\\CMON", "isAT"));
+                System.out.println(WinRegistry.readString(WinRegistry.HKEY_CURRENT_USER, "SOFTWARE\\BSS\\CMON", "isAT",  WinRegistry.KEY_WOW64_64KEY));
             } catch (IllegalAccessException e) {
                 throw new RuntimeException(e);
             } catch (InvocationTargetException e) {
