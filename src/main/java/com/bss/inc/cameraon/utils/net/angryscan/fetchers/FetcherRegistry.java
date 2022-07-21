a022ce4271be399ff96c5f15bad06b95ea92e675
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-21 22:27:17 +0300

    Update-e983023d-e94e-44d2-b524-97bb030edc2c

--- src/main/java/com/bss/inc/cameraon/utils/net/angryscan/fetchers/FetcherRegistry.java
+++ src/main/java/com/bss/inc/cameraon/utils/net/angryscan/fetchers/FetcherRegistry.java
@@ -13,7 +13,7 @@ public class FetcherRegistry {
     private List<FetcherRegistryUpdateListener> updateListeners = new ArrayList<>();
 
     public FetcherRegistry() {
-        createFetchersMap(List.of(new IPFetcher(), MACFetcher));
+        createFetchersMap(List.of(new IPFetcher(), new PingFetcher(),));
         loadSelectedFetchers();
     }
 
