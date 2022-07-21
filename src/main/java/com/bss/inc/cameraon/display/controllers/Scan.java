d505b3d23ac06e16f47597ee723997505f7aa814
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-21 22:32:53 +0300

    Update-dfa1e5a5-4a6f-462f-88b9-9d8628a75d1f

--- src/main/java/com/bss/inc/cameraon/display/controllers/Scan.java
+++ src/main/java/com/bss/inc/cameraon/display/controllers/Scan.java
@@ -36,7 +36,7 @@ public class Scan implements Initializable {
         $UI$01.u_10$1$1(btnClose);
         $UI$01.u_10$1$2(btnMinimize,"mainWindow");
         scanBtn.setOnAction(e -> {
-            
+            UI02.transitionTo();
         });
         UI02.addBar(tasksLeftScan);
         UI02.addButton(scanBtn);
