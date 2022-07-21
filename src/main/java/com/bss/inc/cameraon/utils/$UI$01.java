bb0bed8bfc3e5ef1b2b6a3ba612d19007d62af9d
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-21 13:06:17 +0300

    Update-eab52eda-4938-430c-a654-8131f776f8cf

--- src/main/java/com/bss/inc/cameraon/utils/$UI$01.java
+++ src/main/java/com/bss/inc/cameraon/utils/$UI$01.java
@@ -337,6 +337,18 @@ public class $UI$01 {
         });
         textFields[1].textProperty().addListener((observable, oldValue, newValue) -> {
             if(isCopyingLicenseKey) {return;}
+            if(newValue.split("-").length==5 && newValue.split("-") != null) {
+                isCopyingLicenseKey = true;
+                String[] temp = newValue.split("-");
+                textFields[0].setText(temp[0]);
+                textFields[1].setText(temp[1]);
+                textFields[2].setText(temp[2]);
+                textFields[3].setText(temp[3]);
+                textFields[4].setText(temp[4]);
+                textFields[4].requestFocus();
+                isCopyingLicenseKey = false;
+                return;
+            }
             if(oldValue == null || oldValue == "") {return;}
             if(newValue == "") {textFields[0].requestFocus();}
             if((newValue.length() == 6)) {
