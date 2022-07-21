8a8d3566c65f6cd08f03f50306365b43f08bfb56
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-21 14:32:36 +0300

    Update-3c00091b-36a5-4841-a7a0-83ee2f4fbb20

--- src/main/java/com/bss/inc/cameraon/utils/$UI$01.java
+++ src/main/java/com/bss/inc/cameraon/utils/$UI$01.java
@@ -439,7 +439,7 @@ public class $UI$01 {
         if(licenseHandler.verifyProductKey($UI$01.u_11$1$2(textFields)) != ProductKeyState.KEY_GOOD) {
             u_11$1$3(rectangles);
         } else if(licenseHandler.verifyProductKey($UI$01.u_11$1$2(textFields)) == ProductKeyState.KEY_GOOD) {
-            WinRegistry.writeStringValue(WinRegistry.HKEY_LOCAL_MACHINE, "SOFTWARE\\BSS\\CMON");
+            WinRegistry.writeStringValue(WinRegistry.HKEY_LOCAL_MACHINE, "SOFTWARE\\BSS\\CMON\\isAT");
             for(Rectangle rect : rectangles) {
                 rect.setFill(Color.color(0.1294117647058824d,1.0d,0.3882352941176471d));
             }
