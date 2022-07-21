51bc090956d0cc253e29fcde314f7bffae3cf9ea
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-21 15:24:54 +0300

    Update-34cf039e-4e29-4a9a-94bb-f1b3bae34d05

--- src/main/java/com/bss/inc/cameraon/utils/$UI$01.java
+++ src/main/java/com/bss/inc/cameraon/utils/$UI$01.java
@@ -440,7 +440,9 @@ public class $UI$01 {
         if(licenseHandler.verifyProductKey($UI$01.u_11$1$2(textFields)) != ProductKeyState.KEY_GOOD) {
             u_11$1$3(rectangles);
         } else if(licenseHandler.verifyProductKey($UI$01.u_11$1$2(textFields)) == ProductKeyState.KEY_GOOD) {
-            
+            try{
+                if()
+            }
             try {WinRegistry.writeStringValue(WinRegistry.HKEY_CURRENT_USER, "SOFTWARE\\BSS\\CMON", "isAT", SHA512.getSecureSHA("true&&"+Launcher.getHu().getFileStores().get(0).getUUID(),
                     WinRegistry.readString(WinRegistry.HKEY_CURRENT_USER, "SOFTWARE\\BSS\\CMON", "ST",  WinRegistry.KEY_WOW64_64KEY).getBytes()), WinRegistry.KEY_WOW64_64KEY);}
             catch (IllegalAccessException e) {throw new RuntimeException(e);}
