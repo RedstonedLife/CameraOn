b4bc460aa503c83499c4561d5462c83e3769ba62
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-21 22:32:32 +0300

    Update-730c7676-d456-45a1-9d73-3deb03a27691

--- src/main/java/com/bss/inc/cameraon/display/controllers/Scan.java
+++ src/main/java/com/bss/inc/cameraon/display/controllers/Scan.java
@@ -35,6 +35,7 @@ public class Scan implements Initializable {
         $UI$01.u_04$1$1(prefBtn);
         $UI$01.u_10$1$1(btnClose);
         $UI$01.u_10$1$2(btnMinimize,"mainWindow");
+        
         UI02.addBar(tasksLeftScan);
         UI02.addButton(scanBtn);
         //BNC.getStateMachine().addTransitionListener(this);
