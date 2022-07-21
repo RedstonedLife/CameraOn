4d83148d7496e5538e16522cc290761e938d6623
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-21 13:58:31 +0300

    Update-b6f29dd5-8db2-427b-acd1-280b501dc8bd

--- src/main/java/com/bss/inc/cameraon/display/controllers/LicenseWindow.java
+++ src/main/java/com/bss/inc/cameraon/display/controllers/LicenseWindow.java
@@ -27,7 +27,9 @@ public class LicenseWindow implements Initializable {
         btnClose.setOnAction(e -> MainClass.getWindowManager().changeDialogScene("welcomeWindow", MainClass.class.getResource(FrontendPaths.WELCOME_NA_FXML)));
         $UI$01.u_11$1$1(new TextField[]{ACtf1,ACtf2,ACtf3,ACtf4,ACtf5});
         ActivateBtn.setOnAction(e -> {
-            if(licenseHandler.verifyProductKey())
+            if(licenseHandler.verifyProductKey($UI$01.u_11$1$2({ACtf1,ACtf2,ACtf3,ACtf4,ACtf5}))) {
+                
+            }
         });
         $UI$01.u_10$1$2(btnMinimize, "welcomeWindow");
     }
