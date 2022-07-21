9d2573190c532ff2fdd3db925e92e5773eb6e39b
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-21 14:41:16 +0300

    Update-a958ee80-cd71-4c84-8eb2-9d9ca3cf3cd5

--- src/main/java/com/bss/inc/cameraon/utils/SHA512.java
+++ src/main/java/com/bss/inc/cameraon/utils/SHA512.java
@@ -1,12 +1,15 @@
 package com.bss.inc.cameraon.utils;
 
 import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class SHA512 {
     public static String getSecureSHA(final String string, final byte[] salt) {
         String temp = null;
         try {
             MessageDigest md = MessageDigest.getInstance("SHA-512");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
         }
         return null;
     }
