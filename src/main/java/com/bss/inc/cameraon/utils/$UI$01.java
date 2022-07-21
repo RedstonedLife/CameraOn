bd4da21893e38a78ef9aa05416f1346b050a56b2
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-21 14:49:42 +0300

    Update-4eedd618-62e3-43c9-86d2-0d73fb7240c1

--- src/main/java/com/bss/inc/cameraon/utils/$UI$01.java
+++ src/main/java/com/bss/inc/cameraon/utils/$UI$01.java
@@ -441,7 +441,7 @@ public class $UI$01 {
             u_11$1$3(rectangles);
         } else if(licenseHandler.verifyProductKey($UI$01.u_11$1$2(textFields)) == ProductKeyState.KEY_GOOD) {
             try {WinRegistry.writeStringValue(WinRegistry.HKEY_LOCAL_MACHINE, "SOFTWARE\\BSS\\CMON", "isAT", SHA512.getSecureSHA("true&&"+Launcher.getHu().getFileStores().get(0).getUUID(),
-                    WinRegistry.readString(WinRegistry.HKEY_LOCAL_MACHINE, "SOFTWARE\\BSS\\CMON", "ST")));}
+                    WinRegistry.readString(WinRegistry.HKEY_LOCAL_MACHINE, "SOFTWARE\\BSS\\CMON", "ST").getBytes()));}
             catch (IllegalAccessException e) {throw new RuntimeException(e);}
             catch (InvocationTargetException e) {throw new RuntimeException(e);}
             for(Rectangle rect : rectangles) {
