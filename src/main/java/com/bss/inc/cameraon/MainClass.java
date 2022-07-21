69df051e93f015e6a47c2aee46482192a4cd5602
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-21 22:24:26 +0300

    Update-2ddf26e7-314b-45d0-8dea-d2af905f0137

--- src/main/java/com/bss/inc/cameraon/MainClass.java
+++ src/main/java/com/bss/inc/cameraon/MainClass.java
@@ -59,7 +59,7 @@ public class MainClass extends Application {
         injector = new Injector();
         try {pingerRegistry = new PingerRegistry(config.forScanner(), injector);} catch (ClassNotFoundException e) {throw new RuntimeException(e);}
         scannerDispatcherThreadFactory = new ScannerDispatcherThreadFactory(scanningResults,scanner,stateMachine,config.forScanner());
-        ui02 = new $UI$02(scannerDispatcherThreadFactory,stateMachine,);
+        ui02 = new $UI$02(scannerDispatcherThreadFactory,stateMachine,pingerRegistry,config.forScanner());
 
     }
 
