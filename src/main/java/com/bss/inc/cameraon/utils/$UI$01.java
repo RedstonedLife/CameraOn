feee3fe6f58ad37a41ab37e18f2d847cc95c50e5
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-21 20:05:57 +0300

    Update-8b6d7e9e-5579-451f-83ac-91a8cb6ae544

--- src/main/java/com/bss/inc/cameraon/utils/$UI$01.java
+++ src/main/java/com/bss/inc/cameraon/utils/$UI$01.java
@@ -475,9 +475,9 @@ public class $UI$01 {
     public static boolean u_12$1$1(String ip) {return ipaddressPattern.matcher(ip).matches();}
     public static void u_12$1$2(TextField[] textFields) {
         for(int i=0; i< textFields.length; i++) {
-            textFields[0].textProperty().addListener((observable, oldValue, newValue) -> {
+            textFields[i].textProperty().addListener((observable, oldValue, newValue) -> {
                 if(oldValue == null || oldValue == "") {return;}
-                if(u_12$1$1(newValue)) {textField.setText(newValue);return;}
+                if(u_12$1$1(newValue)) {textFields[i].setText(newValue);return;}
             });
         }
     }
