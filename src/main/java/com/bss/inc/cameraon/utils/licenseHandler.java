6ca873da4f0f03575867750df64d91ed2a87ecdd
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-21 13:47:50 +0300

    Update-ce8649ca-6ba1-4c9b-954e-cbfe12ad4482

--- src/main/java/com/bss/inc/cameraon/utils/licenseHandler.java
+++ src/main/java/com/bss/inc/cameraon/utils/licenseHandler.java
@@ -1,5 +1,19 @@
 package com.bss.inc.cameraon.utils;
 
+import com.weilerhaus.productKeys.impl.BasicProductKeyGenerator;
+import com.weilerhaus.productKeys.impl.beans.BasicProductKeyEncodingData;
+
 public class licenseHandler {
-    
+    private static BasicProductKeyGenerator basicProductKeyGenerator = new BasicProductKeyGenerator(
+            null,
+            new BasicProductKeyEncodingData((byte) 15, (byte) 211, (byte) 68),
+            null,
+            null,
+            new BasicProductKeyEncodingData((byte) 142, (byte) 350, (byte) 17),
+            null,
+            null,
+            null,
+            new BasicProductKeyEncodingData((byte) 302, (byte) 109, (byte) 308),
+            null
+    );
 }
