b334fb5edb0e214ccb35cb41f6330e03cb9e52c3
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-21 14:09:02 +0300

    Update-1424d571-2e47-4b81-8ea7-102136ff443a

--- src/main/java/com/bss/inc/cameraon/display/controllers/LicenseWindow.java
+++ src/main/java/com/bss/inc/cameraon/display/controllers/LicenseWindow.java
@@ -27,7 +27,7 @@ public class LicenseWindow implements Initializable {
         HelpBtn.setCancelButton(true);
         btnClose.setOnAction(e -> MainClass.getWindowManager().changeDialogScene("welcomeWindow", MainClass.class.getResource(FrontendPaths.WELCOME_NA_FXML)));
         $UI$01.u_11$1$1(new TextField[]{ACtf1,ACtf2,ACtf3,ACtf4,ACtf5});
-        ActivateBtn.setOnAction(e -> $UI$01.u_11$1$4(new TextField[]{ACtf1,ACtf2,ACtf3,ACtf4,ACtf5}));
+        ActivateBtn.setOnAction(e -> $UI$01.u_11$1$4(new TextField[]{ACtf1,ACtf2,ACtf3,ACtf4,ACtf5},));
         $UI$01.u_10$1$2(btnMinimize, "welcomeWindow");
     }
 }
