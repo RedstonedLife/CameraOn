a78dfc8e3aef69c88cd9554399efc85d7de50cb0
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-21 15:18:51 +0300

    Update-9ffd3005-6633-4ea1-8b91-b1d1c2fc790d

--- src/main/java/com/bss/inc/cameraon/MainClass.java
+++ src/main/java/com/bss/inc/cameraon/MainClass.java
@@ -79,7 +79,7 @@ public class MainClass extends Application {
     public static void main(String[] args) {
         launch(args);
         try {
-            Preferences.userRoot().node("BasSof")
+            Preferences.userRoot().node("BasSofSol")
         } catch (BackingStoreException e) {
             throw new RuntimeException(e);
         }
