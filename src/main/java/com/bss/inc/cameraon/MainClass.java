60e73eadc24e932d232226ea133035e77c2546e2
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-21 15:36:05 +0300

    Update-c5ed3c82-a4ab-49c0-8e94-4ac58ad73879

--- src/main/java/com/bss/inc/cameraon/MainClass.java
+++ src/main/java/com/bss/inc/cameraon/MainClass.java
@@ -71,7 +71,7 @@ public class MainClass extends Application {
         if(regi.get("ST", null) == null && regi.get("isAT", null) == null) {
             WindowManager.changeDialogScene("welcomeWindow", clazz.getResource(FrontendPaths.WELCOME_NA_FXML));
         } else if(regi.get("ST", null) != null && regi.get("isAT", null) != null) {
-            if(regi.get("isAT", null) != SHA512.getSecureSHA("true&&"+Launcher.getHu().getFileStores().get(0).getUUID()))
+            if(regi.get("isAT", null) != SHA512.getSecureSHA("true&&"+Launcher.getHu().getFileStores().get(0).getUUID(), regi.get("ST", null).getBytes()))
             WindowManager.changeDialogScene("welcomeWindow", clazz.getResource(FrontendPaths.WELCOME_FXML));
         }
         WindowManager.openDialog("welcomeWindow");
