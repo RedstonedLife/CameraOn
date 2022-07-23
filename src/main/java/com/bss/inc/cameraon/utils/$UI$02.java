c1b2363584adb86c204521bc8f99e709923d29e6
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-23 13:54:42 +0300

    Update-969266c4-4ccf-40c7-bcba-ef29026b0e9f

--- src/main/java/com/bss/inc/cameraon/utils/$UI$02.java
+++ src/main/java/com/bss/inc/cameraon/utils/$UI$02.java
@@ -120,7 +120,7 @@ public class $UI$02 implements StateTransitionListener, ScanningProgressCallback
                     resultTable.addOrUpdateResultRow(result);
                 }
                 public void consumeResults(ScanningResult result) {
-                    System.out.println("");
+                    System.out.println("RESULT");
                     resultTable.addOrUpdateResultRow(result);
                 }
             };
