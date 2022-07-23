d0bbfd59a4db219c6c1fd489d5263c25d74bb495
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-23 16:31:25 +0300

    Update-ac513936-264c-4686-a0c6-76119d937dc5

--- src/main/java/com/bss/inc/cameraon/utils/$UI$02.java
+++ src/main/java/com/bss/inc/cameraon/utils/$UI$02.java
@@ -138,8 +138,8 @@ public class $UI$02 implements StateTransitionListener, ScanningProgressCallback
 
     @Override
     public void updateProgress(InetAddress currentAddress, int runningThreads, int percentageComplete) {
-        if (currentAddress != null) System.out.println("Trying " + ((currentAddress == null) ? "N/A" : currentAddress.getHostAddress().toString()) + " | Running Threads: " + String.valueOf(runningThreads) + " | Percent Complete: " + String.valueOf(percentageComplete/100) + "%");
-        try{progressBar.setProgress(percentageComplete/100);}catch (ArithmeticException ignored) {progressBar.setProgress(0.0001d);}
+        if (currentAddress != null) System.out.println("Trying " + ((currentAddress == null) ? "N/A" : currentAddress.getHostAddress().toString()) + " | Running Threads: " + String.valueOf(runningThreads) + " | Percent Complete: " + String.valueOf(100/percentageComplete) + "%");
+        try{if (currentAddress != null) System.out.println("Trying " + ((currentAddress == null) ? "N/A" : currentAddress.getHostAddress().toString()) + " | Running Threads: " + String.valueOf(runningThreads) + " | Percent Complete: " + String.valueOf(100/percentageComplete) + "%");}catch (ArithmeticException ignored) {progressBar.setProgress(0.0001d);}
     }
 }
 
