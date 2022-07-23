2a6f950dee8e4451e4b35c50d7f4b10941273ed4
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-24 01:17:04 +0300

    Update-5e3c85d8-1b9d-4573-8a88-7bf817486ecc

--- src/main/java/org/me/javawsdiscovery/DeviceDiscovery.java
+++ src/main/java/org/me/javawsdiscovery/DeviceDiscovery.java
@@ -3,5 +3,5 @@ package org.me.javawsdiscovery;
 public class DeviceDiscovery {
     public static int WS_DISCOVERY_TIMEOUT = 4000;
     public static int WS_DISCOVERY_PORT = 3702;
-    public static String WS_DISCOVERY_ADDRESS_IP
+    public static String WS_DISCOVERY_ADDRESS_IPv4 = "239.255.255.250";
 }
