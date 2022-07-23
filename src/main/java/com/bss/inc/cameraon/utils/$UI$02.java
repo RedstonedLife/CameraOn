1ddcf6f50e667162e7f55f4c7e5c18dff03dcddc
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-23 16:36:45 +0300

    Update-edb068e9-c66f-41d9-8593-11e40e76d3f7

--- src/main/java/com/bss/inc/cameraon/utils/$UI$02.java
+++ src/main/java/com/bss/inc/cameraon/utils/$UI$02.java
@@ -138,7 +138,7 @@ public class $UI$02 implements StateTransitionListener, ScanningProgressCallback
 
     @Override
     public void updateProgress(InetAddress currentAddress, int runningThreads, int percentageComplete) {
-        try{if (currentAddress != null) System.out.println("Trying " + ((currentAddress == null) ? "N/A" : currentAddress.getHostAddress().toString()) + " | Running Threads: " + String.valueOf(runningThreads) + " | Percent Complete: " + String.valueOf(percentageComplete) + "%");progressBar.setProgress(0.0001d);}catch (ArithmeticException ignored) {progressBar.setProgress(0.0001d);}
+        try{if (currentAddress != null) System.out.println("Trying " + ((currentAddress == null) ? "N/A" : currentAddress.getHostAddress().toString()) + " | Running Threads: " + String.valueOf(runningThreads) + " | Percent Complete: " + String.valueOf(percentageComplete/100) + "%");progressBar.setProgress(0.0001d);}catch (ArithmeticException ignored) {progressBar.setProgress(0.0001d);}
     }
 }
 
