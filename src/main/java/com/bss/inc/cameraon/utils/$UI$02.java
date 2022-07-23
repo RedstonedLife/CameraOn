091d60002702973e4dd63c2fd547f0490e371cfb
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-23 18:53:28 +0300

    Update-da81c319-e46b-48ae-96e4-e03f71e222ac

--- src/main/java/com/bss/inc/cameraon/utils/$UI$02.java
+++ src/main/java/com/bss/inc/cameraon/utils/$UI$02.java
@@ -223,6 +223,15 @@ public class $UI$02 implements StateTransitionListener, ScanningProgressCallback
         Bar.setProgress(1d);
         subBar.setProgress(0.0d);
         subBarTitle.setText("Parsing Results (0/"+(resultTable.getResults().size()-1)+")");
+        double perInc = Math.abs(1.0 / (resultTable.getResults().size()+1));
+        for(int i=0; i<resultTable.getResults().size(); i++) {
+            results.add(resultTable.getResults().get(i));
+            try {
+                TimeUnit.MILLISECONDS.sleep(150);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 
     @Override
