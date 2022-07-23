b7fbe404abca818b584a368ca23cf27a7449a237
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-24 01:18:16 +0300

    Update-f0244d65-c7a3-49e6-964b-f29055282856

--- src/main/java/org/me/javawsdiscovery/DeviceDiscovery.java
+++ src/main/java/org/me/javawsdiscovery/DeviceDiscovery.java
@@ -1,5 +1,6 @@
 package org.me.javawsdiscovery;
 
+import java.net.URL;
 import java.security.SecureRandom;
 import java.util.Random;
 
@@ -11,5 +12,5 @@ public class DeviceDiscovery {
     public static String WS_DISCOVERY_PROBE_MESSAGE = "<soap:Envelope xmlns:soap=\"http://www.w3.org/2003/05/soap-envelope\" xmlns:wsa=\"http://schemas.xmlsoap.org/ws/2004/08/addressing\" xmlns:tns=\"http://schemas.xmlsoap.org/ws/2005/04/discovery\"><soap:Header><wsa:Action>http://schemas.xmlsoap.org/ws/2005/04/discovery/Probe</wsa:Action><wsa:MessageID>urn:uuid:c032cfdd-c3ca-49dc-820e-ee6696ad63e2</wsa:MessageID><wsa:To>urn:schemas-xmlsoap-org:ws:2005:04:discovery</wsa:To></soap:Header><soap:Body><tns:Probe/></soap:Body></soap:Envelope>";
     private static final Random random = new SecureRandom();
 
-    
+    public static Collection<URL>
 }
