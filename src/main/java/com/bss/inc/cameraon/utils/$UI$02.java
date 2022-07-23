678256aaa8919c5ace34ec5e365b2f44ec83d8f2
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-23 13:55:29 +0300

    Update-316083a8-8457-4bca-a191-6bff2b3a335e

--- src/main/java/com/bss/inc/cameraon/utils/$UI$02.java
+++ src/main/java/com/bss/inc/cameraon/utils/$UI$02.java
@@ -120,7 +120,7 @@ public class $UI$02 implements StateTransitionListener, ScanningProgressCallback
                     resultTable.addOrUpdateResultRow(result);
                 }
                 public void consumeResults(ScanningResult result) {
-                    System.out.println("RESULT (IP): " + result.getAddress() + " | IS ALIVE? " + ((result.getType().equals(ScanningResult.ResultType.ALIVE))));
+                    System.out.println("RESULT (IP): " + result.getAddress() + " | IS ALIVE? " + ((result.getType().equals(ScanningResult.ResultType.ALIVE) ? "True" : "False") + " ");
                     resultTable.addOrUpdateResultRow(result);
                 }
             };
