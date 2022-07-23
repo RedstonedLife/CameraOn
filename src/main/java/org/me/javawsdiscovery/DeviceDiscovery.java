2b40d97fa204676ee3c749a3e96032a84120f432
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-24 01:17:30 +0300

    Update-c118b2ab-2769-4ac9-9938-a3db4b874f17

--- src/main/java/org/me/javawsdiscovery/DeviceDiscovery.java
+++ src/main/java/org/me/javawsdiscovery/DeviceDiscovery.java
@@ -5,5 +5,5 @@ public class DeviceDiscovery {
     public static int WS_DISCOVERY_PORT = 3702;
     public static String WS_DISCOVERY_ADDRESS_IPv4 = "239.255.255.250";
     public static String WS_DISCOVERY_ADDRESS_IPv6 = "[FF02::C]";
-    public 
+    public static String WS_DISCOVERY_PROBE_MESSAGE = "<soap:Envelope xmlns:soap=\"http://www.w3.org/2003/05/soap-envelope\" xmlns:wsa=\"http://schemas.xmlsoap.org/ws/2004/08/addressing\" xmlns:tns=\"http://schemas.xmlsoap.org/ws/2005/04/discovery\"><soap:Header><wsa:Action>http://schemas.xmlsoap.org/ws/2005/04/discovery/Probe</wsa:Action><wsa:MessageID>urn:uuid:c032cfdd-c3ca-49dc-820e-ee6696ad63e2</wsa:MessageID><wsa:To>urn:schemas-xmlsoap-org:ws:2005:04:discovery</wsa:To></soap:Header><soap:Body><tns:Probe/></soap:Body></soap:Envelope>";
 }
