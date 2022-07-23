3efa92c5c5b9e817824d71096a5a19670218bf4c
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-23 17:26:24 +0300

    Update-9d65a671-0296-4963-ab94-a4ca41ec66d1

--- src/main/java/com/bss/inc/cameraon/utils/$UI$02.java
+++ src/main/java/com/bss/inc/cameraon/utils/$UI$02.java
@@ -87,7 +87,7 @@ public class $UI$02 implements StateTransitionListener, ScanningProgressCallback
     }
 
     public void transitionTo(final ScanningState state, final StateMachine.Transition transition) {
-        
+        barTitle.setText("");
         if(transition == StateMachine.Transition.INIT)
             return;
         // TODO: Enhance?
