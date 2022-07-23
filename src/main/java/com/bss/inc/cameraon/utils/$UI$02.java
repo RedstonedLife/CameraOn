01f3375d0135d39ebdf8850621e1d1189e0047a8
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-23 18:16:40 +0300

    Update-ff4d6d0e-238a-40e4-9f2c-66a17ee64250

--- src/main/java/com/bss/inc/cameraon/utils/$UI$02.java
+++ src/main/java/com/bss/inc/cameraon/utils/$UI$02.java
@@ -43,6 +43,7 @@ public class $UI$02 implements StateTransitionListener, ScanningProgressCallback
     private Button button;
     private ScannerConfig scannerConfig;
     private String startIpAddress,endIpAddress;
+    private List<String> 
     private ScanFileManager sfm;
 
     public $UI$02(ScannerDispatcherThreadFactory scannerThreadFactory, StateMachine stateMachine,
