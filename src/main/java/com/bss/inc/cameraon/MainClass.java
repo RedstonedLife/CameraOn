7173a7f7618b8a420f127cc04ac62c8a26a76257
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-23 15:23:34 +0300

    Update-7ee19c02-5b81-4e75-8f75-ecd87ca23369

--- src/main/java/com/bss/inc/cameraon/MainClass.java
+++ src/main/java/com/bss/inc/cameraon/MainClass.java
@@ -55,8 +55,8 @@ public class MainClass extends Application {
     static {
         DisplayType = DisplayResultType.valueOf(((JSONObject)Launcher.SettingsContainer.getValue("scanResultDisplay")).getString("displayResult"));
         injector = new Injector();
-        scanner = new Scanner();
         try {pingerRegistry = new PingerRegistry(config.forScanner(), injector);} catch (ClassNotFoundException e) {throw new RuntimeException(e);}
+        scanner = new Scanner();
         scanningResults = new ScanningResultList(stateMachine);
         scannerDispatcherThreadFactory = new ScannerDispatcherThreadFactory(scanningResults,scanner,stateMachine,config.forScanner());
         ui02 = new $UI$02(scannerDispatcherThreadFactory,stateMachine,pingerRegistry,config.forScanner());
