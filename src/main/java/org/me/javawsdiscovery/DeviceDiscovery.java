42c256f383dcfc7c9a9f975830bfcee1c4a40c4f
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-24 01:17:34 +0300

    Update-b69946df-47c7-471a-84e8-1ade72567654

--- src/main/java/org/me/javawsdiscovery/DeviceDiscovery.java
+++ src/main/java/org/me/javawsdiscovery/DeviceDiscovery.java
@@ -6,4 +6,5 @@ public class DeviceDiscovery {
     public static String WS_DISCOVERY_ADDRESS_IPv4 = "239.255.255.250";
     public static String WS_DISCOVERY_ADDRESS_IPv6 = "[FF02::C]";
     public static String WS_DISCOVERY_PROBE_MESSAGE = "<soap:Envelope xmlns:soap=\"http://www.w3.org/2003/05/soap-envelope\" xmlns:wsa=\"http://schemas.xmlsoap.org/ws/2004/08/addressing\" xmlns:tns=\"http://schemas.xmlsoap.org/ws/2005/04/discovery\"><soap:Header><wsa:Action>http://schemas.xmlsoap.org/ws/2005/04/discovery/Probe</wsa:Action><wsa:MessageID>urn:uuid:c032cfdd-c3ca-49dc-820e-ee6696ad63e2</wsa:MessageID><wsa:To>urn:schemas-xmlsoap-org:ws:2005:04:discovery</wsa:To></soap:Header><soap:Body><tns:Probe/></soap:Body></soap:Envelope>";
+    
 }
