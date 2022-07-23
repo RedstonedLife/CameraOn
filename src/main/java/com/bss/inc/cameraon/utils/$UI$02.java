ab89fe37580f216368b198aa4af3761d79955aaf
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-23 17:17:07 +0300

    Update-04a91944-3077-4134-8d20-300af4ee7143

--- src/main/java/com/bss/inc/cameraon/utils/$UI$02.java
+++ src/main/java/com/bss/inc/cameraon/utils/$UI$02.java
@@ -159,7 +159,7 @@ public class $UI$02 implements StateTransitionListener, ScanningProgressCallback
 
     @Override
     public void updateProgress(InetAddress currentAddress, int runningThreads, double percentageComplete) {
-        this.subBarTitle.setText("");
+        this.subBarTitle.setText("Scanning ");
         if(currentAddress!=null) {this.status.appendText(
                 "Trying " +
                         ((currentAddress == null) ? "N/A" : currentAddress.getHostAddress().toString()) +
