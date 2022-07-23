4d4176123500bd32eb38ed713bb8f31e35edf4d1
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-23 17:34:26 +0300

    Update-f843fa58-9962-4670-a00a-ce2068433a93

--- src/main/java/com/bss/inc/cameraon/constants/LoggingConstants.java
+++ src/main/java/com/bss/inc/cameraon/constants/LoggingConstants.java
@@ -6,6 +6,7 @@ import java.time.format.DateTimeFormatter;
 
 public class LoggingConstants {
     public static final String LOG_FOLDER = FilePaths.SETTINGS_FOLDER + "logs/";
+    public static final String SCAN_FOLDER = FilePaths.SETTINGS_FOLDER + "scans/"
     public static final String LOG_LATEST = "latest.log";
     public static final String LOG_CLASSED_FORMAT = "[%s](%s)-%s: %s";
     public static final String LOG_UNCLASSED_FORMAT = "[%s](%s): %s";
