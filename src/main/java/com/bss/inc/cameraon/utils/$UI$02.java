6b589c010c2a024b6a90002e9a0eb54c524320de
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-23 17:20:01 +0300

    Update-33068bf9-e964-461c-b37f-ae8013a46165

--- src/main/java/com/bss/inc/cameraon/utils/$UI$02.java
+++ src/main/java/com/bss/inc/cameraon/utils/$UI$02.java
@@ -169,7 +169,7 @@ public class $UI$02 implements StateTransitionListener, ScanningProgressCallback
             subBar.setProgress(percentageComplete);
         }
         else {
-            double percentage = runningThreads/scannerConfig.maxThreads
+            double percentage = runningThreads/scannerConfig.maxThreads;
             status.appendText();
         }
 
