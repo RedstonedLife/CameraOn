8beac8902bb8db1aea9c8e23437837a6913c3e46
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-24 01:17:48 +0300

    Update-02f56985-2c63-4e47-aabb-88e1de564a55

--- src/main/java/org/me/javawsdiscovery/DeviceDiscovery.java
+++ src/main/java/org/me/javawsdiscovery/DeviceDiscovery.java
@@ -1,10 +1,15 @@
 package org.me.javawsdiscovery;
 
+import java.security.SecureRandom;
+import java.util.Random;
+
 public class DeviceDiscovery {
     public static int WS_DISCOVERY_TIMEOUT = 4000;
     public static int WS_DISCOVERY_PORT = 3702;
     public static String WS_DISCOVERY_ADDRESS_IPv4 = "239.255.255.250";
     public static String WS_DISCOVERY_ADDRESS_IPv6 = "[FF02::C]";
     public static String WS_DISCOVERY_PROBE_MESSAGE = "<soap:Envelope xmlns:soap=\"http://www.w3.org/2003/05/soap-envelope\" xmlns:wsa=\"http://schemas.xmlsoap.org/ws/2004/08/addressing\" xmlns:tns=\"http://schemas.xmlsoap.org/ws/2005/04/discovery\"><soap:Header><wsa:Action>http://schemas.xmlsoap.org/ws/2005/04/discovery/Probe</wsa:Action><wsa:MessageID>urn:uuid:c032cfdd-c3ca-49dc-820e-ee6696ad63e2</wsa:MessageID><wsa:To>urn:schemas-xmlsoap-org:ws:2005:04:discovery</wsa:To></soap:Header><soap:Body><tns:Probe/></soap:Body></soap:Envelope>";
+    private static final Random random = new SecureRandom();
+
     
 }
