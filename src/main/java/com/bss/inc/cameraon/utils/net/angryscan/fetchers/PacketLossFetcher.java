2eb045517a937e89e828c2c2bdfb6c3569930b38
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-24 21:29:07 +0300

    Update-b26d4881-adf5-4450-b5b0-b98a55e29b65

--- src/main/java/com/bss/inc/cameraon/utils/net/angryscan/fetchers/PacketLossFetcher.java
+++ src/main/java/com/bss/inc/cameraon/utils/net/angryscan/fetchers/PacketLossFetcher.java
@@ -1,4 +1,22 @@
 package com.bss.inc.cameraon.utils.net.angryscan.fetchers;
 
-public class PacketLossFetcher {
+import com.bss.inc.cameraon.utils.net.angryscan.config.ScannerConfig;
+import com.bss.inc.cameraon.utils.net.angryscan.state.PingerRegistry;
+
+public class PacketLossFetcher extends PingFetcher {
+
+    public PacketLossFetcher(PingerRegistry pingerRegistry, ScannerConfig scannerConfig) {
+        super(pingerRegistry, scannerConfig);
+    }
+
+    public String getId() {
+        return "fetcher.packetloss";
+    }
+
+    public Object scan(ScanningSubject subject) {
+        PingResult result = executePing(subject);
+        subject.setResultType(result.isAlive() ? ALIVE : DEAD);
+
+        return result.getPacketLoss() + "/" + result.getPacketCount() + " (" + result.getPacketLossPercent() + "%)";
+    }
 }
