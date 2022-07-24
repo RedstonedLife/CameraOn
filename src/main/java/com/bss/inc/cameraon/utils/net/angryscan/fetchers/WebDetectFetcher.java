191898eb2a03fb730023278822912fa68179aa7f
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-24 21:27:03 +0300

    Update-1004f3f4-360f-4bc7-a35d-8a9127f131e5

--- src/main/java/com/bss/inc/cameraon/utils/net/angryscan/fetchers/WebDetectFetcher.java
+++ src/main/java/com/bss/inc/cameraon/utils/net/angryscan/fetchers/WebDetectFetcher.java
@@ -1,2 +1,14 @@
-package com.bss.inc.cameraon.utils.net.angryscan.fetchers;public class WebDetectFetcher {
+package com.bss.inc.cameraon.utils.net.angryscan.fetchers;
+
+import com.bss.inc.cameraon.utils.net.angryscan.config.ScannerConfig;
+
+public class WebDetectFetcher extends PortTextFetcher {
+
+    public WebDetectFetcher(ScannerConfig scannerConfig) {
+        super(scannerConfig, 80, "HEAD /robots.txt HTTP/1.0\r\n\r\n", "^[Ss]erver:\\s+(.*)$");
+    }
+
+    public String getId() {
+        return "fetcher.webDetect";
+    }
 }
