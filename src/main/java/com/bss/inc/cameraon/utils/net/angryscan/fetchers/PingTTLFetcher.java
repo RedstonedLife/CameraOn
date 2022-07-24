68499191a1e294b3be5c003c4c0c0a7d04ba7381
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-24 21:31:05 +0300

    Update-17819cde-6d96-4e50-bf34-d1262022caff

--- src/main/java/com/bss/inc/cameraon/utils/net/angryscan/fetchers/PingTTLFetcher.java
+++ src/main/java/com/bss/inc/cameraon/utils/net/angryscan/fetchers/PingTTLFetcher.java
@@ -1,4 +1,24 @@
 package com.bss.inc.cameraon.utils.net.angryscan.fetchers;
 
-public class PingTTLFetcher {
+import com.bss.inc.cameraon.utils.net.angryscan.ScanningResult;
+import com.bss.inc.cameraon.utils.net.angryscan.ScanningSubject;
+import com.bss.inc.cameraon.utils.net.angryscan.config.ScannerConfig;
+import com.bss.inc.cameraon.utils.net.angryscan.state.PingResult;
+import com.bss.inc.cameraon.utils.net.angryscan.state.PingerRegistry;
+
+public class PingTTLFetcher extends PingFetcher {
+
+    public PingTTLFetcher(PingerRegistry pingerRegistry, ScannerConfig scannerConfig) {
+        super(pingerRegistry, scannerConfig);
+    }
+
+    public String getId() {
+        return "fetcher.ping.ttl";
+    }
+
+    public Object scan(ScanningSubject subject) {
+        PingResult result = executePing(subject);
+        subject.setResultType(result.isAlive() ? ScanningResult.ResultType.ALIVE : ScanningResult.ResultType.DEAD);
+        return result.isAlive() && result.getTTL() > 0 ? result.getTTL() : null;
+    }
 }
