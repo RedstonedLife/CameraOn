1c91787d76ebfd941a7577bdae1ac3c44e3b47b7
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-24 21:34:24 +0300

    Create LastAliveTimeFetcher.java

---
+++ src/main/java/com/bss/inc/cameraon/utils/net/angryscan/fetchers/LastAliveTimeFetcher.java
@@ -0,0 +1,16 @@
+package com.bss.inc.cameraon.utils.net.angryscan.fetchers;
+
+import com.bss.inc.cameraon.utils.net.angryscan.ScanningSubject;
+
+public class LastAliveTimeFetcher extends AbstractFetcher {
+
+    public String getId() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public Object scan(ScanningSubject subject) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+}
