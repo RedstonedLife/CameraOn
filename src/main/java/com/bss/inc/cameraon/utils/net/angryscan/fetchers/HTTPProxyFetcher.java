04f6d7a9f7bff2a789c32dc7e0b722c00b763285
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-24 21:18:41 +0300

    Update-1bc7b316-091b-4466-aad0-7315cf1b27f2

--- src/main/java/com/bss/inc/cameraon/utils/net/angryscan/fetchers/HTTPProxyFetcher.java
+++ src/main/java/com/bss/inc/cameraon/utils/net/angryscan/fetchers/HTTPProxyFetcher.java
@@ -1,5 +1,19 @@
 package com.bss.inc.cameraon.utils.net.angryscan.fetchers;
 
+import com.bss.inc.cameraon.utils.net.angryscan.config.ScannerConfig;
+
 public class HTTPProxyFetcher extends PortTextFetcher {
-    
+    public HTTPProxyFetcher(ScannerConfig scannerConfig) {
+        super(scannerConfig, 3128, "HEAD http://www.google.com HTTP/1.0\r\n\r\n", "^(HTTP/[\\d\\.]+ [23].*)$");
+        this.scanOpenPorts = true;
+    }
+
+    public String getId() {
+        return "fetcher.httpProxy";
+    }
+
+    @Override
+    protected String getResult(Matcher matcher, int port) {
+        return port + ": " + super.getResult(matcher, port);
+    }
 }
