05b8e853226c78b61a4324ab389a4936313ea557
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-24 21:10:33 +0300

    Update-c18c267b-f5c6-4920-ad54-db134d76708c

--- src/main/java/com/bss/inc/cameraon/utils/net/angryscan/config/ComponentRegistry.java
+++ src/main/java/com/bss/inc/cameraon/utils/net/angryscan/config/ComponentRegistry.java
@@ -1,4 +1,30 @@
 package com.bss.inc.cameraon.utils.net.angryscan.config;
 
+import com.bss.inc.cameraon.utils.net.angryscan.di.Injector;
+import com.bss.inc.cameraon.utils.net.angryscan.fetchers.*;
+
 public class ComponentRegistry {
+    public void register(Injector i) throws InstantiationException, IllegalAccessException, ClassNotFoundException {
+        i.register(IPFetcher.class, PingFetcher.class, PingTTLFetcher.class, HostnameFetcher.class, PortsFetcher.class);
+        i.register(MACFetcher.class, (MACFetcher) Class.forName(MACFetcher.class.getPackage().getName() +
+                (Platform.WINDOWS ? ".WinMACFetcher" : Platform.LINUX ? ".LinuxMACFetcher" : ".UnixMACFetcher")).newInstance());
+        i.register(CommentFetcher.class, FilteredPortsFetcher.class, WebDetectFetcher.class, HTTPSenderFetcher.class,
+                NetBIOSInfoFetcher.class, PacketLossFetcher.class, HTTPProxyFetcher.class, MACVendorFetcher.class);
+        i.register(TXTExporter.class, CSVExporter.class, XMLExporter.class, IPListExporter.class, SQLExporter.class);
+    }
+
+    public Injector init() throws Exception {
+        return init(true);
+    }
+
+    public Injector init(boolean withGUI) throws Exception {
+        Injector i = new Injector();
+        new ConfigModule().register(i);
+        new ComponentRegistry().register(i);
+        if (withGUI) {
+            new GUIRegistry().register(i);
+            new PluginLoader().getClasses().forEach(i::require);
+        }
+        return i;
+    }
 }
