8de7941465b12d2bc7fda55df47d3cf9000b18cb
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-24 22:25:18 +0300

    Update-e40bd58d-0952-41a0-85aa-0e9ef92accd6

--- src/main/java/com/bss/inc/cameraon/utils/net/angryscan/fetchers/HTTPSenderFetcher.java
+++ src/main/java/com/bss/inc/cameraon/utils/net/angryscan/fetchers/HTTPSenderFetcher.java
@@ -3,8 +3,8 @@ package com.bss.inc.cameraon.utils.net.angryscan.fetchers;
 import com.bss.inc.cameraon.utils.net.angryscan.config.ScannerConfig;
 
 public class HTTPSenderFetcher extends PortTextFetcher {
-    public HTTPSenderFetcher(ScannerConfig scannerConfig) {
-        super(scannerConfig, 80, "HEAD / HTTP/1.0\r\n\r\n", "Date: (.*)$");
+    public HTTPSenderFetcher() {
+        super( 80, "HEAD / HTTP/1.0\r\n\r\n", "Date: (.*)$");
     }
 
     public String getId() {
