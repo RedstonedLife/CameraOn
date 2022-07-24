66f7d5502300935c2c293d6d9e9ff41727f6bc50
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-24 21:18:49 +0300

    Update-9ae4de6f-4967-4a4d-ac6e-648e9863ddc3

--- src/main/java/com/bss/inc/cameraon/utils/net/angryscan/fetchers/HTTPProxyFetcher.java
+++ src/main/java/com/bss/inc/cameraon/utils/net/angryscan/fetchers/HTTPProxyFetcher.java
@@ -2,6 +2,8 @@ package com.bss.inc.cameraon.utils.net.angryscan.fetchers;
 
 import com.bss.inc.cameraon.utils.net.angryscan.config.ScannerConfig;
 
+import java.util.regex.Matcher;
+
 public class HTTPProxyFetcher extends PortTextFetcher {
     public HTTPProxyFetcher(ScannerConfig scannerConfig) {
         super(scannerConfig, 3128, "HEAD http://www.google.com HTTP/1.0\r\n\r\n", "^(HTTP/[\\d\\.]+ [23].*)$");
