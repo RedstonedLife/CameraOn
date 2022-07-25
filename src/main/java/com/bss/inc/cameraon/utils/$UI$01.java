0e06e7061b388dd9eb3a40cd89a289a8ca37ef3a
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-25 21:04:26 +0300

    Update-62ced262-1ced-40ba-8401-4d0dc2cfabe2

--- src/main/java/com/bss/inc/cameraon/utils/$UI$01.java
+++ src/main/java/com/bss/inc/cameraon/utils/$UI$01.java
@@ -231,7 +231,7 @@ public class $UI$01 {
         for (TextField textField: tfs) {
             textField.textProperty().addListener((observable, oldValue, newValue) -> {
                 if(oldValue == null || oldValue == "") {return;}
-                if(oldValue.length() == 1 && !u_08$1$3("")) {textField.setText("");}
+                if(oldValue.length() == 1) {textField.setText("");}
                 if(!u_08$1$3(newValue)) {textField.setText(oldValue);}
                 if((newValue.length() > 5)) {textField.setText(oldValue);}
             });
