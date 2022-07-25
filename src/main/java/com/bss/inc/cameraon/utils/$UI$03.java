d52a90719b829f88b29f50c8686176b7ec4f17e2
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-25 17:57:07 +0300

    Update-26cb1c9a-95e9-4868-b5c3-b82f62b7e0f7

--- src/main/java/com/bss/inc/cameraon/utils/$UI$03.java
+++ src/main/java/com/bss/inc/cameraon/utils/$UI$03.java
@@ -1,4 +1,10 @@
 package com.bss.inc.cameraon.utils;
 
+import javafx.scene.control.ProgressBar;
+import javafx.scene.text.Text;
+
 public class $UI$03 {
+    private ProgressBar Bar;
+    private ProgressBar subBar;
+    private Text barTitle,subBarTitle;
 }
