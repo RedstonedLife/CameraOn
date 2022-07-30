4f749b1db544b0bb84f31039f613aed1911f7593
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-30 23:34:44 +0300

    DLL Creation

--- BSSLF/src/main/java/com/bss/inc/utils/ProductKeyUtils.c
+++ BSSLF/src/main/java/com/bss/inc/utils/ProductKeyUtils.c
@@ -3,6 +3,7 @@
 #include <String.h>
 #include "com_bss_inc_utils_ProductKeyUtils.h"
 
-JNIEXPORT String JNICALL Java_ProductKeyUtils_buildHexStr(JNIENV *env, jobject obj) {
-    return "Fuck!";
+JNIEXPORT void JNICALL Java_ProductKeyUtils_buildHexStr(JNIEnv *env, jobject obj) {
+	printf("FUCK!");
+    return;
 }
