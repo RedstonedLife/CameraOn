c66d7eeec476ee35f4d6caa081d6af634d37d15b
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-30 23:31:55 +0300

    Update ProductKeyUtils.c

--- BSSLF/src/main/java/com/bss/inc/utils/ProductKeyUtils.c
+++ BSSLF/src/main/java/com/bss/inc/utils/ProductKeyUtils.c
@@ -1,7 +1,7 @@
 #include <jni.h>
 #include <stdio.h>
 #include <String.h>
-#include "ProductKeyUtils.h"
+#include "com_bss_inc_utils_ProductKeyUtils.h"
 
 JNIEXPORT String JNICALL Java_ProductKeyUtils_buildHexStr(JNIENV *env, jobject obj) {
     return "Fuck!";
