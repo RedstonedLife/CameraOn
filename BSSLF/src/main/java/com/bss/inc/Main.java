c2f5a9a21e303b93b070f6f0a911c37228174aef
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-30 23:22:52 +0300

    Update Main.java

--- BSSLF/src/main/java/com/bss/inc/Main.java
+++ BSSLF/src/main/java/com/bss/inc/Main.java
@@ -1,7 +1,12 @@
 package com.bss.inc;
 
 public class Main {
+    private native String buildHexStr();
     public static void main(String[] args) {
         System.out.println("Hello world!");
+    }
+
+    static {
+        System.loadLibrary("ProductKeyUtils");
     }
 }
