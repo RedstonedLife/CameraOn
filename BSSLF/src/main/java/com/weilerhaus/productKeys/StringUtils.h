ea72ded62c9bb10e1876fe9b043479e071150137
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-31 06:15:36 +0300

    Updating

--- BSSLF/src/main/java/com/weilerhaus/productKeys/StringUtils.h
+++ BSSLF/src/main/java/com/weilerhaus/productKeys/StringUtils.h
@@ -1,10 +1,6 @@
-#include <iostream>
 #include <string>
-#include <algorithm>
 #ifndef STRINGUTILS_H
 #define STRINGUTILS_H
-
-const std::string WHITESPACE = " \n\r\t\f\v";
 class StringUtils {
     public:
         std::string ltrim(const std::string &s);
