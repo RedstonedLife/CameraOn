7511df3214feb60aac3f1791aa7bddd940cf4fd0
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-31 07:26:19 +0300

    Update BasicProductKeyStylingWorker.cpp

--- BSSLF/src/main/java/com/weilerhaus/productKeys/BasicProductKeyStylingWorker.cpp
+++ BSSLF/src/main/java/com/weilerhaus/productKeys/BasicProductKeyStylingWorker.cpp
@@ -7,7 +7,21 @@
 
 std::string BasicProductKeyStyling::addStyling(std::string productKey) {
     if((productKey != nullptr) && (StringUtils::trim(productKey).length() > 0)) {
-        std::stringstream streamString;
-        streamString << StringUtils::toUpper(StringUtils::trim(productKey));
+        std::string streamString;
+        streamString = StringUtils::toUpper(StringUtils::trim(productKey));
+        int tmpCharsSinceDash = 0;
+        for(int n = (streamString.length() -1); n >= 0; n--) {
+            if(n >= 5) {
+                if(tmpCharsSinceDash>=5) {
+                    streamString.insert(n,'-');
+                    tmpCharsSinceDash=0;
+                } else {tmpCharsSinceDash++;}} else {break;}
+        }
+        return streamString;
     }
+    return productKey;
+}
+std::string BasicProductKeyStyling::removeStyling(std::string productKey) {
+    if((productKey != nullptr) && (StringUtils::trim(productKey) > 0)) {return productKey.replace('-','');}
+    return productKey;
 }
