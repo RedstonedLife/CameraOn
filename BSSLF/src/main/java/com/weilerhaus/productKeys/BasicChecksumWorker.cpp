8457cbc58e09679d5e3f2e768b9657b009a459b8
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-31 06:28:14 +0300

    Update BasicChecksumWorker.cpp

--- BSSLF/src/main/java/com/weilerhaus/productKeys/BasicChecksumWorker.cpp
+++ BSSLF/src/main/java/com/weilerhaus/productKeys/BasicChecksumWorker.cpp
@@ -3,50 +3,32 @@
 #include <vector>
 #include "ProductKeyUtilsImpl.h"
 #include "BasicChecksumWorker.h"
+#include "StringUtils.h"
 
 std::string BasicChecksumWorker::buildProductKeyChecksum(std::string productKey) {
-    if((productKey != nullptr) && (productKey))
+    if((productKey != nullptr) && (StringUtils::trim(productKey).length() > 0)) {
+        productKey = StringUtils::trim(productKey);
+        int left = 86;
+        int right = 175;
+        for(int n=0; n < productKey.length(); n++) {
+            right = right + (int)productKey[n];
+            if (right > 255) {right -= 255;}
+            left += right;
+            if (left > 255) {left -= 255;}
+        }
+        bhs(4, (left << 8) + right);
+    }
+    return nullptr;
 }
-
-
-std::string BasicChecksumWorker::buildProductKeyChecksum(std::string productKey)
-    {
-        if ((productKey != nullptr) && (productKey.trim().length() > 0))
+bool BasicChecksumWorker::verifyProductKeyChecksum(std::string productKey) {
+    if((productKey != nullptr) && (StringUtils::trim(productKey).length() > 0)) {
+        productKey = productKey.trim();
+        if (productKey.length() > 4)
         {
-            productKey = productKey.trim();
-            int left = 86;
-            int right = 175;
-            for (int n = 0; n < productKey.length(); n++)
-            {
-                right = right + (int)productKey[n];
-                if (right > 255)
-                {
-                    right -= 255;
-                }
-                left += right;
-                if (left > 255)
-                {
-                    left -= 255;
-                }
-
-            }
-            return bhs(4, (left << 8) + right);
+            std::string checkSum = productKey.substr(productKey.length() - 4);
+            productKey = productKey.substr(0,productKey.length() - 4);
+            return (checkSum.compare(buildProductKeyChecksum(productKey)) != 0);
         }
-        return nullptr;
     }
-    bool BasicChecksumWorker::verifyProductKeyChecksum(std::string productKey)
-    {
-        if ((productKey != nullptr) && (productKey.trim().length() > 0))
-        {
-            productKey = productKey.trim();
-            if (productKey.length() > 4)
-            {
-                // Extract the last four characters as they are the checksum.
-                std::string checkSum = productKey.substr(productKey.length() - 4);
-                productKey = productKey.substr(0,productKey.length() - 4);
-                // Compare the extracted checksum against the generated checksum for the key string.
-                return (checkSum.compare(buildProductKeyChecksum(productKey)) != 0);
-            }
-        }
-        return false;
-    }
+    return false;
+}
