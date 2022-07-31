8e061f7cea99f57725cc235b1fbb4e722a9a171c
Tal A. Baskin <talbaskin.business@gmail.com>
2022-07-31 06:20:34 +0300

    Update StringUtils.cpp

--- BSSLF/src/main/java/com/weilerhaus/productKeys/StringUtils.cpp
+++ BSSLF/src/main/java/com/weilerhaus/productKeys/StringUtils.cpp
@@ -3,3 +3,16 @@
 #include <algorithm>
 #include "StringUtils.h"
 
+const std::string WHITESPACE = " \n\r\t\f\v";
+
+std::string StringUtils::ltrim(const std::string &s) {
+    size_t start = s.find_first_not_of(WHITESPACE);
+    return (start==std::string::npos) ? "" : s.substr(start);
+}
+std::string StringUtils::rtrim(const std::string &s) {
+    size_t end = s.find_last_not_of(WHITESPACE);
+    return (end == std::string::npos) ? "" : s.substr(0, end + 1);
+}
+std::string StringUtils::trim(const std::string &s) {
+    return rtrim(ltrim(s));
+}
