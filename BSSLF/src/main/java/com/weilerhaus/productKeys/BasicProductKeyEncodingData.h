4eb27466921a5c336dd35f39376652aca8f43910
Tal A. Baskin <talbaskin.business@gmail.com>
2022-08-03 09:07:11 +0300

    Update

---
+++ BSSLF/src/main/java/com/weilerhaus/productKeys/BasicProductKeyEncodingData.h
@@ -0,0 +1,24 @@
+#include "ProductKeyEncodingData.h"
+#include "ProductKeySectionWorker.h"
+
+#ifndef BASICPRODUCTKEYENCODINGDATA_H
+#define BASICPRODUCTKEYENCODINGDATA_H
+
+class BasicProductKeyEncodingData : public ProductKeyEncodingData {
+private:
+    int a;
+    int b;
+    int c;
+    friend int ProductKeySectionWorker::buildProductKeySection(long seed, ProductKeyEncodingData& encodingData);
+public:
+    ProductKeyEncodingData(int a, int b, int c) {
+        this->a=a;
+        this->b=b;
+        this->c=c;
+    }
+    int getA() {return a;}
+    int getB() {return b;}
+    int getC() {return c;}
+};
+
+#endif //BASICPRODUCTKEYENCODINGDATA_H
--- BSSLF/src/main/java/com/weilerhaus/productKeys/ProductKeyEncodingData.h
+++ BSSLF/src/main/java/com/weilerhaus/productKeys/ProductKeyEncodingData.h
@@ -1,19 +1,5 @@
 #include "ProductKeySectionWorker.h"
 #ifndef PRODUCTKEYENCODINGDATA_H
 #define PRODUCTKEYENCODINGDATA_H
-
-class ProductKeyEncodingData {
-    private:
-    int a;
-    int b;
-    int c;
-    friend int ProductKeySectionWorker::buildProductKeySection(long seed, ProductKeyEncodingData& encodingData);
-    public:
-    ProductKeyEncodingData(int a, int b, int c);
-    void setInts(int a, int b, int c);
-    int getA() {return a;}
-    int getB() {return b;}
-    int getC() {return c;}
-};
-
+class ProductKeyEncodingData {};
 #endif
